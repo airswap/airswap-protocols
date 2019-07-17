35b3bd53e801b8cf2b98f00e218f93a768e1a6ee
Ethan Wessel <ethan.wessel@fluidity.io>
2019-07-17 17:33:17 -0400

    common imports

---
+++ contracts/common/contracts/CommonImports.sol
@@ -0,0 +1,9 @@
+Pragma solidity 0.5.10;
+import "@airswap/tokens/contracts/FungibleToken.sol";
+import "@airswap/tokens/contracts/NonFungibleToken.sol";
+import "@airswap/swap/contracts/Swap.sol";
+import "@airswap/common/libraries/Transfers.sol";
+import "@airswap/tokens/contracts/WETH9.sol";
+import "@airswap/indexer/contracts/Indexer.sol";
+import "@airswap/delegate/contracts/Delegate.sol";
+import "@gnosis.pm/mock-contract/contracts/MockContract.sol";
