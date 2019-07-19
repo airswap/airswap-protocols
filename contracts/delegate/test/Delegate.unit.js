486b13450be2e76e5e1232ec2133d8fed7662377
Ethan Wessel <ethan.wessel@fluidity.io>
2019-07-19 16:01:39 -0400

    yarn lint --fix

--- contracts/delegate/test/Delegate.unit.js
+++ contracts/delegate/test/Delegate.unit.js
@@ -3,13 +3,17 @@ const Swap = artifacts.require('Swap')
 const FungibleToken = artifacts.require('FungibleToken')
 const MockContract = artifacts.require('MockContract')
 const abi = require('ethereumjs-abi')
-const { equal, notEqual, passes, emitted } = require('@airswap/test-utils').assert
+const {
+  equal,
+  notEqual,
+  passes,
+  emitted,
+} = require('@airswap/test-utils').assert
 const { takeSnapshot, revertToSnapShot } = require('@airswap/test-utils').time
 
 const EMPTY_ADDRESS = '0x0000000000000000000000000000000000000000'
 
 contract.only('Delegate Unit Tests', async accounts => {
-
   let delegate
   let mockSwap
   const DELEGATE_TOKEN = accounts[9]
@@ -28,15 +32,14 @@ contract.only('Delegate Unit Tests', async accounts => {
   })
 
   before('deploy Delegate', async () => {
-
     mockSwap = await MockContract.new()
     delegate = await Delegate.new(mockSwap.address)
   })
 
   describe('Test initial values', async () => {
     it('Test initial Swap Contract', async () => {
-      let val = await delegate.swapContract.call();
-      equal(val, mockSwap.address, 'swap address is incorrect');
+      let val = await delegate.swapContract.call()
+      equal(val, mockSwap.address, 'swap address is incorrect')
     })
   })
 
@@ -45,82 +48,158 @@ contract.only('Delegate Unit Tests', async accounts => {
       let newSwap = await MockContract.new()
       await delegate.setSwapContract(newSwap.address)
       let val = await delegate.swapContract.call()
-      notEqual(val, mockSwap.address, "the swap contract has not changed")
-      equal(val, newSwap.address, "the swap contract has not changed to the right value")
+      notEqual(val, mockSwap.address, 'the swap contract has not changed')
+      equal(
+        val,
+        newSwap.address,
+        'the swap contract has not changed to the right value'
+      )
     })
 
     it('Test setRule', async () => {
-      let trx = await delegate.setRule(DELEGATE_TOKEN, CONSUMER_TOKEN, MAX_DELEGATE_AMOUNT, PRICE_COEF, EXP)
+      let trx = await delegate.setRule(
+        DELEGATE_TOKEN,
+        CONSUMER_TOKEN,
+        MAX_DELEGATE_AMOUNT,
+        PRICE_COEF,
+        EXP
+      )
 
       //check if rule has been added
       let rule = await delegate.rules.call(DELEGATE_TOKEN, CONSUMER_TOKEN)
-      equal(rule[0].toNumber(), MAX_DELEGATE_AMOUNT, "max delegate amount is incorrectly saved")
-      equal(rule[1].toNumber(), PRICE_COEF, "price coef is incorrectly saved")
-      equal(rule[2].toNumber(), EXP, "price exp is incorrectly saved")
-      
+      equal(
+        rule[0].toNumber(),
+        MAX_DELEGATE_AMOUNT,
+        'max delegate amount is incorrectly saved'
+      )
+      equal(rule[1].toNumber(), PRICE_COEF, 'price coef is incorrectly saved')
+      equal(rule[2].toNumber(), EXP, 'price exp is incorrectly saved')
+
       //check emitted event
-      emitted(trx, 'SetRule', (e) => {
-        return e.delegateToken === DELEGATE_TOKEN &&
-        e.consumerToken === CONSUMER_TOKEN &&
-        e.maxDelegateAmount.toNumber() === MAX_DELEGATE_AMOUNT &&
-        e.priceCoef.toNumber() === PRICE_COEF &&
-        e.priceExp.toNumber() === EXP
+      emitted(trx, 'SetRule', e => {
+        return (
+          e.delegateToken === DELEGATE_TOKEN &&
+          e.consumerToken === CONSUMER_TOKEN &&
+          e.maxDelegateAmount.toNumber() === MAX_DELEGATE_AMOUNT &&
+          e.priceCoef.toNumber() === PRICE_COEF &&
+          e.priceExp.toNumber() === EXP
+        )
       })
     })
 
     it('Test unsetRule', async () => {
-      let trx = await delegate.setRule(DELEGATE_TOKEN, CONSUMER_TOKEN, MAX_DELEGATE_AMOUNT, PRICE_COEF, EXP)
+      let trx = await delegate.setRule(
+        DELEGATE_TOKEN,
+        CONSUMER_TOKEN,
+        MAX_DELEGATE_AMOUNT,
+        PRICE_COEF,
+        EXP
+      )
 
       //check emitted event
-      emitted(trx, 'SetRule', (e) => {
-        return e.delegateToken === DELEGATE_TOKEN &&
-        e.consumerToken === CONSUMER_TOKEN &&
-        e.maxDelegateAmount.toNumber() === MAX_DELEGATE_AMOUNT &&
-        e.priceCoef.toNumber() === PRICE_COEF &&
-        e.priceExp.toNumber() === EXP
+      emitted(trx, 'SetRule', e => {
+        return (
+          e.delegateToken === DELEGATE_TOKEN &&
+          e.consumerToken === CONSUMER_TOKEN &&
+          e.maxDelegateAmount.toNumber() === MAX_DELEGATE_AMOUNT &&
+          e.priceCoef.toNumber() === PRICE_COEF &&
+          e.priceExp.toNumber() === EXP
+        )
       })
 
       trx = await delegate.unsetRule(DELEGATE_TOKEN, CONSUMER_TOKEN)
 
       //check that the rule has been removed
       let rule = await delegate.rules.call(DELEGATE_TOKEN, CONSUMER_TOKEN)
-      equal(rule[0].toNumber(), 0, "max delgate amount is incorrectly saved")
-      equal(rule[1].toNumber(), 0, "price coef is incorrectly saved")
-      equal(rule[2].toNumber(), 0, "price exp is incorrectly saved")
-      
+      equal(rule[0].toNumber(), 0, 'max delgate amount is incorrectly saved')
+      equal(rule[1].toNumber(), 0, 'price coef is incorrectly saved')
+      equal(rule[2].toNumber(), 0, 'price exp is incorrectly saved')
+
       //check emitted event
-      emitted(trx, 'UnsetRule', (e) => {
-        return e.delegateToken === DELEGATE_TOKEN &&
-        e.consumerToken === CONSUMER_TOKEN
+      emitted(trx, 'UnsetRule', e => {
+        return (
+          e.delegateToken === DELEGATE_TOKEN &&
+          e.consumerToken === CONSUMER_TOKEN
+        )
       })
     })
   })
 
   describe('Test getBuyQuote', async () => {
     it('test when delegate does not exist', async () => {
-      const NON_EXISTENT_DELEGATE_TOKEN = accounts[7];
-      let val = await delegate.getBuyQuote.call(1234, NON_EXISTENT_DELEGATE_TOKEN, CONSUMER_TOKEN)
-      equal(val.toNumber(), 0, "no quote should be available if a delegate does not exist")
+      const NON_EXISTENT_DELEGATE_TOKEN = accounts[7]
+      let val = await delegate.getBuyQuote.call(
+        1234,
+        NON_EXISTENT_DELEGATE_TOKEN,
+        CONSUMER_TOKEN
+      )
+      equal(
+        val.toNumber(),
+        0,
+        'no quote should be available if a delegate does not exist'
+      )
     })
 
     it('test when delegate amount is greater than max delegate amount', async () => {
-      await delegate.setRule(DELEGATE_TOKEN, CONSUMER_TOKEN, MAX_DELEGATE_AMOUNT, PRICE_COEF, EXP)
-      let val = await delegate.getBuyQuote.call(MAX_DELEGATE_AMOUNT + 1, DELEGATE_TOKEN, CONSUMER_TOKEN)
-      equal(val.toNumber(), 0, "no quote should be available if delegate amount is greater than delegate max amount")
+      await delegate.setRule(
+        DELEGATE_TOKEN,
+        CONSUMER_TOKEN,
+        MAX_DELEGATE_AMOUNT,
+        PRICE_COEF,
+        EXP
+      )
+      let val = await delegate.getBuyQuote.call(
+        MAX_DELEGATE_AMOUNT + 1,
+        DELEGATE_TOKEN,
+        CONSUMER_TOKEN
+      )
+      equal(
+        val.toNumber(),
+        0,
+        'no quote should be available if delegate amount is greater than delegate max amount'
+      )
     })
 
     it('test when delegate amount is 0', async () => {
-      await delegate.setRule(DELEGATE_TOKEN, CONSUMER_TOKEN, MAX_DELEGATE_AMOUNT, PRICE_COEF, EXP)
-      let val = await delegate.getBuyQuote.call(0, DELEGATE_TOKEN, CONSUMER_TOKEN)
-      equal(val.toNumber(), 0, "no quote should be available if delegate amount is 0")
+      await delegate.setRule(
+        DELEGATE_TOKEN,
+        CONSUMER_TOKEN,
+        MAX_DELEGATE_AMOUNT,
+        PRICE_COEF,
+        EXP
+      )
+      let val = await delegate.getBuyQuote.call(
+        0,
+        DELEGATE_TOKEN,
+        CONSUMER_TOKEN
+      )
+      equal(
+        val.toNumber(),
+        0,
+        'no quote should be available if delegate amount is 0'
+      )
     })
 
     it('test a successful call', async () => {
-      await delegate.setRule(DELEGATE_TOKEN, CONSUMER_TOKEN, MAX_DELEGATE_AMOUNT, PRICE_COEF, EXP)
-      let val = await delegate.getBuyQuote.call(1234, DELEGATE_TOKEN, CONSUMER_TOKEN)
+      await delegate.setRule(
+        DELEGATE_TOKEN,
+        CONSUMER_TOKEN,
+        MAX_DELEGATE_AMOUNT,
+        PRICE_COEF,
+        EXP
+      )
+      let val = await delegate.getBuyQuote.call(
+        1234,
+        DELEGATE_TOKEN,
+        CONSUMER_TOKEN
+      )
       //TODO: @dmosites should the getBuyQuote() return with an exponent or a whole number?
       //1234 * PRICE_COEF * 10^(-EXP)
-      equal(val.toNumber(), 5332114, "no quote should be available if delegate amount is 0")
+      equal(
+        val.toNumber(),
+        5332114,
+        'no quote should be available if delegate amount is 0'
+      )
     })
   })
 })
