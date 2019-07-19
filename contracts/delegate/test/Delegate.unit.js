a950ad6bf449f498ad0164423ed2b4ac030e0929
Ethan Wessel <ethan.wessel@fluidity.io>
2019-07-19 12:38:27 -0400

    unit test

--- contracts/delegate/test/Delegate.unit.js
+++ contracts/delegate/test/Delegate.unit.js
@@ -54,6 +54,7 @@ contract.only('Delegate Unit Tests', async accounts => {
 
       //check if rule has been added
       let rule = await delegate.rules.call(DELEGATE_TOKEN, CONSUMER_TOKEN)
+      equal(rule[0].toNumber(), MAX_DELEGATE_AMOUNT, "max delegate amount is incorrectly saved")
       equal(rule[1].toNumber(), PRICE_COEF, "price coef is incorrectly saved")
       equal(rule[2].toNumber(), EXP, "price exp is incorrectly saved")
       
@@ -68,7 +69,30 @@ contract.only('Delegate Unit Tests', async accounts => {
     })
 
     it('Test unsetRule', async () => {
+      let trx = await delegate.setRule(DELEGATE_TOKEN, CONSUMER_TOKEN, MAX_DELEGATE_AMOUNT, PRICE_COEF, EXP)
 
+      //check emitted event
+      emitted(trx, 'SetRule', (e) => {
+        return e.delegateToken === DELEGATE_TOKEN &&
+        e.consumerToken === CONSUMER_TOKEN &&
+        e.maxDelegateAmount.toNumber() === MAX_DELEGATE_AMOUNT &&
+        e.priceCoef.toNumber() === PRICE_COEF &&
+        e.priceExp.toNumber() === EXP
+      })
+
+      trx = await delegate.unsetRule(DELEGATE_TOKEN, CONSUMER_TOKEN)
+
+      //check that the rule has been removed
+      let rule = await delegate.rules.call(DELEGATE_TOKEN, CONSUMER_TOKEN)
+      equal(rule[0].toNumber(), 0, "max delgate amount is incorrectly saved")
+      equal(rule[1].toNumber(), 0, "price coef is incorrectly saved")
+      equal(rule[2].toNumber(), 0, "price exp is incorrectly saved")
+      
+      //check emitted event
+      emitted(trx, 'UnsetRule', (e) => {
+        return e.delegateToken === DELEGATE_TOKEN &&
+        e.consumerToken === CONSUMER_TOKEN
+      })
     })
   })
 })
