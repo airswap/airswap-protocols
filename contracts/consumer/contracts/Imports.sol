33a34ffd3bcaabdc40442af544b9ba9a5709a155
Alice Henshaw <henshawalice@gmail.com>
2019-07-22 16:19:12 +0100

    making minimal changes to find break

--- contracts/consumer/contracts/Imports.sol
+++ contracts/consumer/contracts/Imports.sol
@@ -1,3 +1,8 @@
 pragma solidity 0.5.10;
 
-import "@airswap/common/contracts/CommonImports.sol";
+import "@airswap/common/libraries/Transfers.sol";
+import "@airswap/indexer/contracts/Indexer.sol";
+import "@airswap/delegate/contracts/Delegate.sol";
+import "@airswap/swap/contracts/Swap.sol";
+import "@airswap/tokens/contracts/FungibleToken.sol";
+import "@gnosis.pm/mock-contract/contracts/MockContract.sol";
