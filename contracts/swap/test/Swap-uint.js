853b3d788310873d7201449d1d7d2c8c0784a1d8
Ethan Wessel <ethan.wessel@fluidity.io>
2019-07-29 15:00:08 -0400

    template tests

--- contracts/swap/test/Swap-uint.js
+++ contracts/swap/test/Swap-uint.js
@@ -30,9 +30,112 @@ contract('Swap Unit Tests', async () => {
   })
 
   describe('Test swap', async () => {
+    it('test when order is expired', async () => {
+    })
+
+    it('test when order is taken', async () => {
+    })
+
+    it('test when order is canceled', async () => {
+    })
+
+    it('test when order nonce is too low', async () => {
+    })
+
+    it('test when taker is an empty address', async () => {
+    })
+
+    it('test when order is not specified', async () => {
+    })
+
+    it('test when order is specified, but not authorized', async () => {
+    })
+
+    it('test when order is specified, and is authorized', async () => {
+    })
+
+    it('test when order signature.v is 0, and the order maker is unauthorized', async () => {
+    })
+
+    it('test when order signature.v is 0, and the order maker is authorized', async () => {
+    })
+
+    it('test when order signature.v is not 0, and the order maker is unauthorized', async () => {
+    })
+
+    it('test when order signature.v is not 0, the order make is authorized, and the signature is invalid', async () => {
+    })
+
+    it('test when order signature.v is not 0, the order make is authorized, and the signature is valid', async () => {
+    })
+
+    it('test when order taker token is an empty address, and the value is not the expected value to be sent', async () => {
+    })
+
+    it('test when order taker token is an empty address, and the value is the expected value to be sent', async () => {
+    })
+
+    it('tes when order taker token is not an empty address, and the value is not the expected value to be sent', async () => {
+    })
+
+    it('test when order taker token is not an empty address, and the value is the expected value to be sent', async () => {
+    })
+
+    it('test when order affiliate is an empty address', async () => {
+    })
+
+    it('test when order affiliate is an not an empty address', async () => {
+    })
   })
 
   describe('Test swapSimple', async () => {
+    it('test when the order is unavailable', async () => {
+    })
+
+    it('test when the order has expired', async () => {
+    })
+    
+    it('test when the nonce is too low', async () => {
+    })
+
+    it('test when the taker address is the empty address', async () => {
+    })
+
+    it('test when taker address is not the empty address and the message sender is the taker wallet', async () => {
+    })
+
+    it('test when taker address is not the empty address and the message sender is not the taker wallet', async () => {
+    })
+
+    it('test when taker address is not the empty address and the message sender is not the taker wallet, and the taker wallet is unauthorized', async () => {
+    })
+
+    it('test when taker address is not the empty address and the message sender is not the taker wallet, and the taker wallet is authorized', async () => {
+    })
+
+    it('test when v == 0, and maker wallet is unauthorized', async () => {
+    })
+
+    it('test when v == 0, and maker wallet is authorized', async () => {
+    })
+
+    it('test when v != 0, and the signature is invalid', async () => {
+    })
+
+    it('test when v != 0, and the signature is valid', async () => {
+    })
+
+    it('test when taker token is the empty address, and the value is not set', async () => {
+    })
+
+    it('test when taker token is the empty address, and the value is set', async () => {
+    })
+
+    it('test when taker token is not the empty address, and the value is set', async () => {
+    })
+
+    it('test when taker token is not the empty address, and the value is not set', async () => {
+    })
   })
 
   descrive('Test cancel', async () => {
