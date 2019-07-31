946fee3e0c089c00df3e11ceac011fbfdec91fe6
Alice Henshaw <henshawalice@gmail.com>
2019-07-31 11:52:03 +0100

    constructor tests

--- contracts/indexer/test/Indexer-unit.js
+++ contracts/indexer/test/Indexer-unit.js
@@ -2,7 +2,13 @@ const Indexer = artifacts.require('Indexer')
 const MockContract = artifacts.require('MockContract')
 const FungibleToken = artifacts.require('FungibleToken')
 
-const { emitted, reverted, equal, ok } = require('@airswap/test-utils').assert
+const {
+  getResult,
+  emitted,
+  reverted,
+  equal,
+  ok,
+} = require('@airswap/test-utils').assert
 const { balances } = require('@airswap/test-utils').balances
 const { getTimestampPlusDays } = require('@airswap/test-utils').time
 const { intents } = require('@airswap/indexer-utils')
@@ -47,7 +53,33 @@ contract('Indexer Unit Tests', async accounts => {
   before('Setup contracts', async () => {
     await setupMockToken()
     indexer = await Indexer.new(stakingTokenAddress, MIN_STAKE_250)
+    indexerAddress = indexer.address
   })
 
+  describe('Check constructor', async () => {
+    it('should set the staking token address correctly', async () => {
+      const actualAddress = await indexer.stakeToken()
+      equal(
+        actualAddress,
+        stakingTokenAddress,
+        'Staking token was set incorrectly'
+      )
+    })
 
+    it('should set the staking minimum correctly', async () => {
+      const actualMinimum = await indexer.stakeMinimum()
+      equal(actualMinimum, MIN_STAKE_250, 'Staking minimum was set incorrectly')
+    })
+
+    it('should emit an event in the constructor', async () => {
+      // create a new indexer
+      const newIndexer = await Indexer.new(stakingTokenAddress, MIN_STAKE_250)
+
+      // get the tx hash and get the transaction result from it
+      let txHash = newIndexer.transactionHash
+      result = await getResult(newIndexer, txHash)
+
+      emitted(result, 'SetStakeMinimum')
+    })
+  })
 })
