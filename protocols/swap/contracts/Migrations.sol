aa0c141d97f523a75a07b11a81c052a9eb631a25
poiresel <deepa@fluidity.io>
2019-08-12 08:55:20 -0400

    add updated Migrations and new packages

--- protocols/swap/contracts/Migrations.sol
+++ protocols/swap/contracts/Migrations.sol
@@ -1,8 +1,8 @@
-pragma solidity >=0.4.21 <0.6.0;
+pragma solidity 0.5.10;
 
 contract Migrations {
   address public owner;
-  uint public last_completed_migration;
+  uint public lastCompletedMigration;
 
   constructor() public {
     owner = msg.sender;
@@ -13,11 +13,11 @@ contract Migrations {
   }
 
   function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
+    lastCompletedMigration = completed;
   }
 
-  function upgrade(address new_address) public restricted {
-    Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
+  function upgrade(address newAddress) public restricted {
+    Migrations upgraded = Migrations(newAddress);
+    upgraded.setCompleted(lastCompletedMigration);
   }
 }
