eeaedcc94aeaf100507f746f8040c9addb05da45
Alice Henshaw <henshawalice@gmail.com>
2019-08-12 10:56:47 +0100

    first draft of peer factory

---
+++ examples/peer/contracts/PeerFactory.sol
@@ -0,0 +1,32 @@
+/*
+  Copyright 2019 Swap Holdings Ltd.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+pragma solidity 0.5.10;
+pragma experimental ABIEncoderV2;
+
+import "@airswap/peer/contracts/Peer.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract PeerFactory {
+
+  mapping(address => bool) public trustedPeers;
+
+  function deployTrustedPeer(address initialSwapContract, address owner) public returns (address) {
+    address newPeer = address(new Peer(initialSwapContract, owner));
+    trustedPeers[newPeer] = true;
+    return newPeer;
+  }
+}
