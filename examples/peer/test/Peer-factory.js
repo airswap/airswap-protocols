3942a624c429972d04aa39ba6705087dc1a7c938
Alice Henshaw <henshawalice@gmail.com>
2019-08-12 13:58:09 +0100

    WIP tests for deploying peer

--- examples/peer/test/Peer-factory.js
+++ examples/peer/test/Peer-factory.js
@@ -1,11 +1,12 @@
 const PeerFactory = artifacts.require('PeerFactory')
+const Peer = artifacts.require('Peer')
 const { takeSnapshot, revertToSnapShot } = require('@airswap/test-utils').time
 const { EMPTY_ADDRESS } = require('@airswap/order-utils').constants
 const {
   reverted,
   passes,
-  isTrue,
   equal,
+  emitted,
 } = require('@airswap/test-utils').assert
 
 contract('Peer Factory Tests', async accounts => {
@@ -46,7 +47,7 @@ contract('Peer Factory Tests', async accounts => {
       )
     })
 
-    it('should deploy a peer, return the address, and update the mapping', async () => {
+    it('should return address, emit event and update the mapping', async () => {
       // return the address
       let peerAddress = await peerFactory.deployTrustedPeer.call(
         swapContractOne,
@@ -64,9 +65,40 @@ contract('Peer Factory Tests', async accounts => {
       )
       passes(tx)
 
+      // emitted event
+      emitted(tx, 'NewTrustedPeer', event => {
+        return (
+          event.peer === peerAddress &&
+          event.swap === swapContractOne &&
+          event.owner === peerOwnerOne
+        )
+      })
+
       // mapping has been updated
       isTrustedPeer = await peerFactory.trustedPeers.call(peerAddress)
-      isTrue(isTrustedPeer)
+      equal(isTrustedPeer, true)
+    })
+
+    it('should create delegate with the correct values', async () => {
+      // return the address
+      let peerAddress = await peerFactory.deployTrustedPeer.call(
+        swapContractTwo,
+        peerOwnerTwo
+      )
+
+      // deploy peer
+      await peerFactory.deployTrustedPeer(swapContractTwo, peerOwnerTwo)
+      let isTrustedPeer = await peerFactory.trustedPeers.call(peerAddress)
+      equal(isTrustedPeer, true)
+
+      // get the swap and owner values of the peer
+      let peer = Peer.at(peerAddress)
+      let actualSwap = await peer.swapContract.call()
+      let actualOwner = await peer.owner.call()
+
+      // check they are correct
+      equal(swapContractTwo, actualSwap, 'Peer has incorrect swap address')
+      equal(peerOwnerTwo, actualOwner, 'Peer has incorrect owner address')
     })
   })
 })
