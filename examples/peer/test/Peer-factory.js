426bd37cd4c2672d779bd4489235e773152b8ae5
Alice Henshaw <henshawalice@gmail.com>
2019-08-12 13:36:12 +0100

    deployment test

--- examples/peer/test/Peer-factory.js
+++ examples/peer/test/Peer-factory.js
@@ -1,7 +1,12 @@
 const PeerFactory = artifacts.require('PeerFactory')
 const { takeSnapshot, revertToSnapShot } = require('@airswap/test-utils').time
 const { EMPTY_ADDRESS } = require('@airswap/order-utils').constants
-const { reverted } = require('@airswap/test-utils').assert
+const {
+  reverted,
+  passes,
+  isTrue,
+  equal,
+} = require('@airswap/test-utils').assert
 
 contract('Peer Factory Tests', async accounts => {
   const swapContractOne = accounts[0]
@@ -39,6 +44,29 @@ contract('Peer Factory Tests', async accounts => {
         peerFactory.deployTrustedPeer(EMPTY_ADDRESS, peerOwnerOne),
         'Provide a swap address'
       )
+    })
+
+    it('should deploy a peer, return the address, and update the mapping', async () => {
+      // return the address
+      let peerAddress = await peerFactory.deployTrustedPeer.call(
+        swapContractOne,
+        peerOwnerOne
+      )
+
+      // before calling the function, the mapping is false
+      let isTrustedPeer = await peerFactory.trustedPeers.call(peerAddress)
+      equal(isTrustedPeer, false)
+
+      // successful tx
+      let tx = await peerFactory.deployTrustedPeer(
+        swapContractOne,
+        peerOwnerOne
+      )
+      passes(tx)
+
+      // mapping has been updated
+      isTrustedPeer = await peerFactory.trustedPeers.call(peerAddress)
+      isTrue(isTrustedPeer)
     })
   })
 })
