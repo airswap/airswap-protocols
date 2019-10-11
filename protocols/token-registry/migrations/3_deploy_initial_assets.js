d00da733628af1f95be73c429f678a49071a2617
poiresel <deepa.s.sathaye@gmail.com>
2019-10-11 15:51:55 -0400

    resolve migrations

--- protocols/token-registry/migrations/3_deploy_initial_assets.js
+++ protocols/token-registry/migrations/3_deploy_initial_assets.js
@@ -1,18 +1,12 @@
-const CryptoKittyAsset = artifacts.require('CryptoKittyAsset')
+const PartialKittyCoreAsset = artifacts.require('PartialKittyCoreAsset')
 const ERC20Asset = artifacts.require('ERC20Asset')
 const ERC721Asset = artifacts.require('ERC721Asset')
 const USDTAsset = artifacts.require('USDTAsset')
 const TokenRegistry = artifacts.require('TokenRegistry')
 
 module.exports = deployer => {
-  deployer.deploy(CryptoKittyAsset)
+  deployer.deploy(PartialKittyCoreAsset)
   deployer.deploy(ERC20Asset)
   deployer.deploy(ERC721Asset)
   deployer.deploy(USDTAsset)
-
-  // add all 4 of these contracts into the TokenRegistry
-  TokenRegistry.addToRegistry('0x9a20483d', CryptoKittyAsset)
-  TokenRegistry.addToRegistry('0x277f8169', ERC20Asset)
-  TokenRegistry.addToRegistry('0x80ac58cd', ERC721Asset)
-  TokenRegistry.addToRegistry('', USDTAsset)
 }
