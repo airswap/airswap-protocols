ab615733519c03028527507ce64f8b54f9348098
Ethan Wessel <ethan.wessel@fluidity.io>
2019-10-28 14:55:18 -0400

    rinkeby network

--- source/indexer/truffle-config.js
+++ source/indexer/truffle-config.js
@@ -18,11 +18,22 @@ module.exports = {
       gas: 0xfffffffffff,
       gasPrice: 0x01,
     },
+    rinkeby: {
+      network_id: 4,
+      provider: () => new HDWalletProvider(
+        process.env.MNEMONIC,
+        "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY
+      ),
+      from: 
+      gasPrice: 2290000000, // currently 2.29 Gwei
+      gas: 4898551 // may need to change
+    }
   },
   compilers: {
     solc: {
       version: '0.5.12',
-      optimization: false,
+      optimization: true,
+      runs: 200
     },
   },
 }
