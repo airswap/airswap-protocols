380d6ed1c538c94a18de0dd041f3d15008416e3c
poiresel <deepa.s.sathaye@gmail.com>
2019-10-29 11:34:28 -0400

    remove types

--- source/swap/contracts/Imports.sol
+++ source/swap/contracts/Imports.sol
@@ -3,7 +3,6 @@ pragma solidity 0.5.12;
 import "@airswap/tokens/contracts/FungibleToken.sol";
 import "@airswap/tokens/contracts/NonFungibleToken.sol";
 import "@airswap/tokens/contracts/OMGToken.sol";
-import "@airswap/types/contracts/Types.sol";
 import "@gnosis.pm/mock-contract/contracts/MockContract.sol";
 
 contract Imports {}
