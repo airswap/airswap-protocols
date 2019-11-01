2540d337683cbfa8397dc796f20db8bd2f0c01dc
Ethan Wessel <ethan.wessel@fluidity.io>
2019-11-01 13:15:54 -0400

    update sol lint ignore

--- source/types/truffle-config.js
+++ source/types/truffle-config.js
@@ -1,5 +1,10 @@
+require('dotenv').config()
+const HDWalletProvider = require('truffle-hdwallet-provider')
+
 module.exports = {
+  contracts_directory: './flat',
   networks: {
+    contracts_directory: './flat',
     development: {
       host: '127.0.0.1',
       port: 8545,
@@ -14,11 +19,23 @@ module.exports = {
       gas: 0xfffffffffff,
       gasPrice: 0x01,
     },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNEMONIC,
+          'https://rinkeby.infura.io/v3/' + process.env.INFURA_API_KEY
+        ),
+      network_id: 4,
+    },
   },
   compilers: {
     solc: {
       version: '0.5.12',
       optimization: false,
     },
+  },
+  plugins: ['truffle-verify'],
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_API_KEY,
   },
 }
