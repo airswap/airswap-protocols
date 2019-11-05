384151436b56dbccf13a77253a728d9ba2779ba1
Alice Henshaw <henshawalice@gmail.com>
2019-11-05 11:04:00 -0500

    update truffle config and package.json

--- source/delegate/truffle-config.js
+++ source/delegate/truffle-config.js
@@ -1,4 +1,8 @@
+require('dotenv').config()
+const HDWalletProvider = require('truffle-hdwallet-provider')
+
 module.exports = {
+  contracts_directory: './flatten',
   networks: {
     development: {
       host: '127.0.0.1',
@@ -14,11 +18,23 @@ module.exports = {
       gas: 0xfffffffffff,
       gasPrice: 0x01,
     },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNEMONIC,
+          'https://rinkeby.infura.io/v3/' + process.env.INFURA_API_KEY
+        ),
+      network_id: 4,
+    },
   },
   compilers: {
     solc: {
       version: '0.5.12',
       optimization: false,
     },
+  },
+  plugins: ['truffle-verify', 'truffle-flatten'],
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_API_KEY,
   },
 }
