b9c4c3757c088c6146ec5b62e9e30ded4e19e581
Alice Henshaw <henshawalice@gmail.com>
2019-11-20 09:30:37 -0500

    Adding explanation of use to the mock contract

--- source/wrapper/contracts/HelperMock.sol
+++ source/wrapper/contracts/HelperMock.sol
@@ -5,6 +5,16 @@ import "@airswap/types/contracts/Types.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import "./Wrapper.sol";
 
+/**
+  * @title HelperMock: This mock contract has been created to test the Wrapper.
+  *
+  * In the final lines of Wrapper.swap ETH is transferred to the sender of the order,
+  * having completed a swap in which the sender received WETH. If this ETH transfer fails
+  * the transaction must revert to prevent ETH trapped in the Wrapper. This situation
+  * occurs when the ETH recipient is a non-payable contract that therefore rejects
+  * the ETH.
+  *
+  */
 contract HelperMock {
 
   Wrapper public wrapper;
