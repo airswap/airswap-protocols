de2379ebe439023909c4bdae5b0307e0f03b61fd
Ethan Wessel <ethan.wessel@fluidity.io>
2019-11-26 15:04:01 -0500

    remove unused colorings

--- scripts/identify_dependencies.py
+++ scripts/identify_dependencies.py
@@ -4,14 +4,8 @@ import json
 import sys
 
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 pp = pprint.PrettyPrinter(indent=2)
 DEV_DEP = "devDependencies"
