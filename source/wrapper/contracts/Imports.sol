fab012f78ce6fe4e526e97d7a7f139f00fb8e657
Alice Henshaw <henshawalice@gmail.com>
2019-11-27 12:08:47 -0500

    unit tests for new function

--- source/wrapper/contracts/Imports.sol
+++ source/wrapper/contracts/Imports.sol
@@ -3,6 +3,8 @@ pragma solidity 0.5.12;
 import "@airswap/tokens/contracts/FungibleToken.sol";
 import "@airswap/tokens/contracts/WETH9.sol";
 import "@airswap/swap/contracts/Swap.sol";
+import "@airswap/delegate/contracts/Delegate.sol";
+import "@airswap/indexer/contracts/Indexer.sol";
 import "@gnosis.pm/mock-contract/contracts/MockContract.sol";
 
 contract Imports {}
