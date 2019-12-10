76a7dbca09439af6d4711e6cb370946663eaee1e
Ethan Wessel <ethan.wessel@fluidity.io>
2019-12-10 12:38:44 -0500

    remove truffle generated text

--- utils/deployer/truffle-config.js
+++ utils/deployer/truffle-config.js
@@ -6,9 +6,9 @@ module.exports = {
   contracts_directory: './flatten',
   networks: {
     development: {
-      host: '127.0.0.1', // Localhost (default: none)
-      port: 8545, // Standard Ethereum port (default: none)
-      network_id: '*', // Any network (default: none)
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*',
     },
     mainnet: {
       provider: () =>
