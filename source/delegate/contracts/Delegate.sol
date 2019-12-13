d4225dc057a344c4633b0023639bbda3948d3445
Ethan Wessel <ethan.wessel@fluidity.io>
2019-12-13 18:09:07 -0500

    update natspecs

--- source/delegate/contracts/Delegate.sol
+++ source/delegate/contracts/Delegate.sol
@@ -428,7 +428,7 @@ contract Delegate is IDelegate, Ownable {
   }
 
   /**
-    * @notice Calculate the signer param (amount) for a given sender param and price
+    * @notice Calculate the signer amount for a given sender param and price
     * @param senderAmount uint256 The amount the delegate would send in the swap
     * @param priceCoef uint256 Coefficient of the token price defined in the rule
     * @param priceExp uint256 Exponent of the token price defined in the rule
@@ -451,7 +451,7 @@ contract Delegate is IDelegate, Ownable {
   }
 
   /**
-    * @notice Calculate the sender param (amount) for a given signer param and price
+    * @notice Calculate the sender amount for a given signer param and price
     * @param signerAmount uint256 The amount the signer would send in the swap
     * @param priceCoef uint256 Coefficient of the token price defined in the rule
     * @param priceExp uint256 Exponent of the token price defined in the rule
