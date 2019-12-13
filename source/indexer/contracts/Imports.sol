1fa8c63364517e1493099c93ab953813706be215
Don Mosites <don.mosites@fluidity.io>
2019-12-12 20:18:20 -0500

    Move DelegateFactory to Delegate package

--- source/indexer/contracts/Imports.sol
+++ source/indexer/contracts/Imports.sol
@@ -3,7 +3,7 @@ pragma solidity 0.5.12;
 import "@airswap/tokens/contracts/FungibleToken.sol";
 import "@airswap/index/contracts/Index.sol";
 import "@gnosis.pm/mock-contract/contracts/MockContract.sol";
-import "@airswap/delegate-factory/contracts/DelegateFactory.sol";
+import "@airswap/delegate/contracts/DelegateFactory.sol";
 import "@airswap/swap/contracts/Swap.sol";
 import "@airswap/types/contracts/Types.sol";
 
--- utils/deployer/contracts/Imports.sol
+++ utils/deployer/contracts/Imports.sol
@@ -1,7 +1,7 @@
 pragma solidity 0.5.12;
 
 //Import all the contracts desired to be deployed
-import "@airswap/delegate-factory/contracts/DelegateFactory.sol";
+import "@airswap/delegate/contracts/DelegateFactory.sol";
 import "@airswap/indexer/contracts/Indexer.sol";
 import "@airswap/swap/contracts/Swap.sol";
 import "@airswap/types/contracts/Types.sol";
