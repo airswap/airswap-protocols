d9ac7507d19052adfbc087170339136e6ff2f075
Alice Henshaw <henshawalice@gmail.com>
2019-12-18 09:50:51 -0500

    affiliate check of token

--- source/swap/contracts/Swap.sol
+++ source/swap/contracts/Swap.sol
@@ -155,7 +155,7 @@ contract Swap is ISwap {
     );
 
     // Transfer token from signer to affiliate if specified.
-    if (order.affiliate.amount != 0) {
+    if (order.affiliate.token != address(0)) {
       transferToken(
         order.signer.wallet,
         order.affiliate.wallet,
