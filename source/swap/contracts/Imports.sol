438492c7c3f142a916fa7cba9c49c5e04a039c87
poiresel <deepa.s.sathaye@gmail.com>
2020-01-06 16:34:13 -0500

    add handler tests back into swap repo and update imports

--- source/swap/contracts/Imports.sol
+++ source/swap/contracts/Imports.sol
@@ -1,9 +1,16 @@
 pragma solidity 0.5.12;
 
 import "@airswap/tokens/contracts/FungibleToken.sol";
+import "@airswap/tokens/contracts/OMGToken.sol";
+import "@airswap/tokens/contracts/NonFungibleToken.sol";
+import "@airswap/tokens/contracts/AdaptedKittyERC721.sol";
+import "@airswap/tokens/contracts/MintableERC1155Token.sol";
 import "@airswap/types/contracts/Types.sol";
 import "@airswap/transfers/contracts/TransferHandlerRegistry.sol";
 import "@airswap/transfers/contracts/handlers/ERC20TransferHandler.sol";
+import "@airswap/transfers/contracts/handlers/ERC721TransferHandler.sol";
+import "@airswap/transfers/contracts/handlers/ERC1155TransferHandler.sol";
+import "@airswap/transfers/contracts/handlers/KittyCoreTransferHandler.sol";
 import "@gnosis.pm/mock-contract/contracts/MockContract.sol";
 
 contract Imports {}
