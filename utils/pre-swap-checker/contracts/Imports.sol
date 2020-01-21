31906d853e60d06e14bd62f89cbd6305a4a5acf4
poiresel <deepa.s.sathaye@gmail.com>
2020-01-21 16:26:43 -0500

    remove unneeded import in Imports.sol

--- utils/pre-swap-checker/contracts/Imports.sol
+++ utils/pre-swap-checker/contracts/Imports.sol
@@ -4,7 +4,6 @@ import "@airswap/tokens/contracts/FungibleToken.sol";
 import "@airswap/tokens/contracts/NonFungibleToken.sol";
 import "@airswap/tokens/contracts/WETH9.sol";
 import "@airswap/swap/contracts/Swap.sol";
-import "@airswap/transfers/contracts/TransferHandlerRegistry.sol";
 import "@airswap/transfers/contracts/handlers/ERC20TransferHandler.sol";
 import "@airswap/transfers/contracts/handlers/ERC721TransferHandler.sol";
 import "@airswap/wrapper/contracts/Wrapper.sol";
