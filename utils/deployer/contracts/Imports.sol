a66f510a9935f05eb7da6d79c1cf57d77fc70b25
poiresel <deepa.s.sathaye@gmail.com>
2020-01-24 10:39:20 -0500

    publishing updates to deployer including transfers

--- utils/deployer/contracts/Imports.sol
+++ utils/deployer/contracts/Imports.sol
@@ -6,5 +6,10 @@ import "@airswap/indexer/contracts/Indexer.sol";
 import "@airswap/swap/contracts/Swap.sol";
 import "@airswap/types/contracts/Types.sol";
 import "@airswap/wrapper/contracts/Wrapper.sol";
+import "@airswap/transfers/contracts/TransferHandlerRegistry.sol";
+import "@airswap/transfers/contracts/handlers/ERC1155TransferHandler.sol";
+import "@airswap/transfers/contracts/handlers/ERC20TransferHandler.sol";
+import "@airswap/transfers/contracts/handlers/ERC721TransferHandler.sol";
+import "@airswap/transfers/contracts/handlers/KittyCoreTransferHandler.sol";
 
 contract Imports {}
