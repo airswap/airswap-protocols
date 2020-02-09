5d4fce34af0f3111b2b4490e717edac036fdd462
Don Mosites <don.mosites@fluidity.io>
2020-02-09 16:18:06 -0500

    Adding new packages clients, constants, and utils

---
+++ utils/clients/src/Delegate.ts
@@ -0,0 +1,97 @@
+/*
+  Copyright 2020 Swap Holdings Ltd.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+import { ethers } from 'ethers'
+import { chainIds, chainNames, MIN_CONFIRMATIONS } from '@airswap/constants'
+
+const DelegateContract = require('@airswap/delegate/build/contracts/Delegate.json')
+
+export class Delegate {
+  locator: string
+  chainId: string
+  contract: any
+
+  constructor(
+    locator: string,
+    chainId = chainIds.RINKEBY,
+    signerOrProvider?: ethers.Signer | ethers.providers.Provider
+  ) {
+    this.locator = locator
+    this.chainId = chainId
+    this.contract = new ethers.Contract(
+      locator,
+      DelegateContract.abi,
+      signerOrProvider ||
+        ethers.getDefaultProvider(chainNames[chainId].toLowerCase())
+    )
+  }
+
+  async getWallet(): Promise<string> {
+    return await this.contract.tradeWallet()
+  }
+
+  async getMaxQuote(signerToken: string, senderToken: string): Promise<any> {
+    const result = await this.contract.getMaxQuote(signerToken, senderToken)
+    return {
+      signer: {
+        token: signerToken,
+        amount: result.signerAmount.toString(),
+      },
+      sender: {
+        token: senderToken,
+        amount: result.senderAmount.toString(),
+      },
+    }
+  }
+
+  async getSignerSideQuote(
+    senderAmount: string,
+    senderToken: string,
+    signerToken: string
+  ): Promise<any> {
+    const signerAmount = await this.contract.getSignerSideQuote(
+      senderAmount,
+      senderToken,
+      signerToken
+    )
+    if (signerAmount.isZero()) {
+      throw new Error('Not quoting for the requested pair')
+    }
+    return {
+      signer: {
+        token: signerToken,
+        amount: signerAmount.toString(),
+      },
+      sender: {
+        token: senderToken,
+        amount: senderAmount,
+      },
+    }
+  }
+
+  async provideOrder(order: any, signer?: ethers.Signer): Promise<string> {
+    let contract = this.contract
+    if (!this.contract.signer) {
+      if (signer === null) {
+        throw new Error('Signer must be provided')
+      }
+      contract = new ethers.Contract(this.locator, DelegateContract.abi, signer)
+    }
+    const tx = await contract.provideOrder(order)
+    await tx.wait(MIN_CONFIRMATIONS)
+    return tx.hash
+  }
+}
---
+++ utils/constants/index.ts
@@ -0,0 +1,114 @@
+/*
+  Copyright 2020 Swap Holdings Ltd.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+export const chainIds: { [key: string]: string } = {
+  MAINNET: '1',
+  RINKEBY: '4',
+}
+
+export const chainNames: { [key: string]: string } = {
+  '1': 'MAINNET',
+  '4': 'RINKEBY',
+}
+
+export const tokenKinds: { [key: string]: string } = {
+  ERC20: '0x36372b07',
+  ERC721: '0x80ac58cd',
+  ERC1155: '0xd9b67a26',
+  CKITTY: '0x9a20483d',
+}
+
+export const tokenKindNames: { [key: string]: string } = {
+  '0x36372b07': 'ERC20',
+  '0x80ac58cd': 'ERC721',
+  '0xd9b67a26': 'ERC1155',
+  '0x9a20483d': 'CKITTY',
+}
+
+export const protocols: { [key: string]: string } = {
+  SERVER: '0x0000',
+  DELEGATE: '0x0001',
+}
+
+export const protocolNames: { [key: string]: string } = {
+  '0x0000': 'SERVER',
+  '0x0001': 'DELEGATE',
+}
+
+export const signatureTypes: { [key: string]: string } = {
+  INTENDED_VALIDATOR: '0x00',
+  SIGN_TYPED_DATA: '0x01',
+  PERSONAL_SIGN: '0x45',
+}
+
+export const rinkebyTokens: { [key: string]: any } = {
+  DAI: {
+    address: '0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea',
+    decimals: 18,
+  },
+  WETH: {
+    address: '0xc778417e063141139fce010982780140aa0cd5ab',
+    decimals: 18,
+  },
+  AST: {
+    address: '0xcc1cbd4f67cceb7c001bd4adf98451237a193ff8',
+    decimals: 4,
+  },
+}
+
+export const stakingTokenAddresses: { [key: string]: any } = {
+  '1': '0x27054b13b1b798b345b591a4d22e6562d47ea75a',
+  '4': '0xcc1cbd4f67cceb7c001bd4adf98451237a193ff8',
+}
+
+export const deltaBalanceAddresses: { [key: string]: any } = {
+  '1': '0x5dfe850d4b029c25c7ef9531ec9986c97d90300f',
+  '4': '0xa1e2c4132cbd33c3876e1254143a850466c97e32',
+}
+
+export const etherscanDomains: { [key: string]: any } = {
+  '1': 'etherscan.io',
+  '4': 'rinkeby.etherscan.io',
+}
+
+export const defaults: { [key: string]: any } = {
+  Party: {
+    kind: '0x36372b07',
+    wallet: '0x0000000000000000000000000000000000000000',
+    token: '0x0000000000000000000000000000000000000000',
+    amount: '0',
+    id: '0',
+  },
+}
+
+export const INDEX_HEAD = '0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF'
+
+export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
+
+export const LOCATOR_ZERO =
+  '0x0000000000000000000000000000000000000000000000000000000000000000'
+
+export const MAX_LOCATORS = 10
+
+export const MAX_APPROVAL_AMOUNT = '90071992547409910000000000'
+
+export const MIN_CONFIRMATIONS = 2
+
+export const DEFAULT_PORT = 3000
+
+export const REQUEST_TIMEOUT = 4000
+
+export const SECONDS_IN_DAY = 86400
