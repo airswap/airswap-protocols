b400fbafa0ac4653571a0775e50cf501b9fd8991
Alice Henshaw <henshawalice@gmail.com>
2020-02-20 14:51:36 -0500

    added dev note to new interface

--- source/tokens/contracts/interfaces/IAdaptedKittyERC721.sol
+++ source/tokens/contracts/interfaces/IAdaptedKittyERC721.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.0;
 
 /**
  * @title Interface for the AdaptedKittyERC721 contract
- *
+ * @dev This matches the interface for KittyCore
  */
 
 interface IAdaptedKittyERC721 {
@@ -12,6 +12,5 @@ interface IAdaptedKittyERC721 {
   function approve(address to, uint256 tokenId) external;
   function kittyIndexToApproved(uint256 tokenId) external view returns (address);
   function transferFrom(address from, address to, uint256 tokenId) external;
-  function mint(address to, uint256 tokenId) external returns (bool);
 
 }
