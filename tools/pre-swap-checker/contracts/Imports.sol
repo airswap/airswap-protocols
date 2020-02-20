58119045dc272c9a7872c46ebc335894cb63aec6
Ethan Wessel <ethan.wessel@fluidity.io>
2020-02-20 14:29:38 -0500

    fix 1155 import

--- tools/pre-swap-checker/contracts/Imports.sol
+++ tools/pre-swap-checker/contracts/Imports.sol
@@ -6,6 +6,7 @@ import "@airswap/tokens/contracts/WETH9.sol";
 import "@airswap/swap/contracts/Swap.sol";
 import "@airswap/transfers/contracts/handlers/ERC20TransferHandler.sol";
 import "@airswap/transfers/contracts/handlers/ERC721TransferHandler.sol";
+import "@airswap/transfers/contracts/handlers/ERC1155TransferHandler.sol";
 import "@airswap/wrapper/contracts/Wrapper.sol";
 import "@airswap/indexer/contracts/Indexer.sol";
 import "@airswap/delegate/contracts/Delegate.sol";
