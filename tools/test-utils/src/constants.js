1aa972f8a8a83e84a2eed6341865cdce79a5cf25
Don Mosites <don.mosites@fluidity.io>
2020-03-10 15:38:37 -0400

    Adding new files for test-utils

---
+++ tools/test-utils/src/constants.js
@@ -0,0 +1,31 @@
+/*
+  Copyright 2020 Swap Holdings Ltd.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+module.exports = {
+  SECONDS_IN_DAY: 86400,
+  EMPTY_ADDRESS: '0x0000000000000000000000000000000000000000',
+  HEAD: '0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF',
+  KNOWN_GANACHE_WALLET: '0xe092b1fa25DF5786D151246E492Eed3d15EA4dAA',
+  LOCATORS: 0,
+  SCORES: 1,
+  NEXTID: 2,
+  ONE_ETH: 1000000000000000000,
+  ERC721_INTERFACE_ID: '0x80ac58cd',
+  ERC20_INTERFACE_ID: '0x36372b07',
+  ERC1155_INTERFACE_ID: '0xd9b67a26',
+  CK_INTERFACE_ID: '0x9a20483d',
+  GANACHE_PROVIDER: 'http://127.0.0.1:8545',
+}
