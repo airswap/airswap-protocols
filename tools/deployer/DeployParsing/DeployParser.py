3db1c01f961a80ea932146934faf1b7066e71b9c
Ethan Wessel <ethan.wessel@fluidity.io>
2020-03-11 12:50:31 -0400

    Make class a static function

--- tools/deployer/DeployParsing/DeployParser.py
+++ tools/deployer/DeployParsing/DeployParser.py
@@ -7,31 +7,30 @@ CHAIN_ID = {
     'goerli': '5',
     'kovan': '42',
 }
+MIGRATION_SUB_IDX = 17
 
-class DeployParser:
 
-    def parse(self, file_input):
-        migration_components = []
+def parse(file_input):
+    migration_components = []
 
-        with open(file_input) as data:
-            for line in data:
-                cleaned_line = line.rstrip()
-                if line[:17] == "MIGRATION_STATUS:":
-                    migration_components.append(cleaned_line[17:])
-                else:
-                    print(cleaned_line)
+    with open(file_input) as data:
+        for line in data:
+            cleaned_line = line.rstrip()
+            if line[:MIGRATION_SUB_IDX] == "MIGRATION_STATUS:":
+                migration_components.append(cleaned_line[MIGRATION_SUB_IDX:])
+            else:
+                print(cleaned_line)
 
-        for component in migration_components:
-            json_obj = json.loads(component)
-            if json_obj['status'] == 'preMigrate':
-                network = json_obj['data']['network']
-                print("Network: " + network + ", " + CHAIN_ID[network])
-            elif json_obj['status'] == 'deployed':
-                contract_name = json_obj['data']['contract']['contractName']
-                contract_address = json_obj['data']['contract']['address']
-                print(contract_name + ": " + contract_address)
+    for component in migration_components:
+        json_obj = json.loads(component)
+        if json_obj['status'] == 'preMigrate':
+            network = json_obj['data']['network']
+            print("Network: " + network + ", " + CHAIN_ID[network])
+        elif json_obj['status'] == 'deployed':
+            contract_name = json_obj['data']['contract']['contractName']
+            contract_address = json_obj['data']['contract']['address']
+            print(contract_name + ": " + contract_address)
 
 
 if __name__ == "__main__":
-    parser = DeployParser()
-    parser.parse("input.txt")
+    parse("input.txt")
