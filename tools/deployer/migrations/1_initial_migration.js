51baf473410e980da886b62beac6adc6baea01bc
Ethan Wessel <ethan.wessel@fluidity.io>
2020-03-11 10:46:02 -0400

    Automate add deploys.json

--- tools/deployer/migrations/1_initial_migration.js
+++ tools/deployer/migrations/1_initial_migration.js
@@ -57,7 +57,9 @@ module.exports = async(deployer, network) => {
   await Validator.link("Types", Types.address)
   await deployer.deploy(Validator, WETH_ADDRESS)
 
-  //Deploy Contract-Created Contracts that also need to be verified
-  await deployer.deploy(Index)
-  await deployer.deploy(Delegate, Swap.address, Indexer.address, EMPTY_ADDRESS, EMPTY_ADDRESS, '0x0001')
+  //Deploy Factory-Created Contracts that also need to be verified
+  if (network !== "DEVELOPMENT") {
+    await deployer.deploy(Index)
+    await deployer.deploy(Delegate, Swap.address, Indexer.address, EMPTY_ADDRESS, EMPTY_ADDRESS, '0x0001')
+  }
 };
