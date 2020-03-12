e8c536a2baee53f7fdd0ed3478a31d0c20907f83
Ethan Wessel <ethan.wessel@fluidity.io>
2020-03-12 09:41:13 -0400

    broke function down into smaller functions

--- tools/deployer/DeployParser.py
+++ tools/deployer/DeployParser.py
@@ -19,18 +19,27 @@ CONTRACT_DIR = {
     'Wrapper': '../../source/wrapper/deploys.json'
 }
 
-
-def parse(file_input):
+def get_migration_components(file_input):
     # separate the migration json and regular output
     migration_components = []
+    deploy_components = []
     with open(file_input) as data:
         for line in data:
             cleaned_line = line.rstrip()
             if line[:MIGRATION_SUB_IDX] == "MIGRATION_STATUS:":
                 migration_components.append(cleaned_line[MIGRATION_SUB_IDX:])
             else:
-                print(cleaned_line)
+                deploy_components.append(cleaned_line)
 
+    # write cleaned lines back to file
+    with open(file_input, 'w') as data:
+        string = '\n'.join(deploy_components)
+        data.write(string)
+        print(string)
+
+    return migration_components
+
+def collect_deployment_information(migration_components):
     # collect all the new deploy information
     network = "development"
     deploy_data = {}
@@ -46,6 +55,9 @@ def parse(file_input):
             contract_address = json_obj['data']['contract']['address']
             deploy_data[contract_name] = contract_address
 
+    return network, deploy_data
+
+def update_deploy_jsons(network, deploy_data):
     # go through all deploys.json and update them
     for contract_name, deploy_file in CONTRACT_DIR.items():
         with open(deploy_file, "r+") as data:
@@ -56,6 +68,11 @@ def parse(file_input):
             # jump to beginning of file and write
             data.seek(0)
             data.write(json.dumps(json_obj, indent=2))
+
+def parse(file_input):
+    migration_components = get_migration_components(file_input)
+    network, deploy_data = collect_deployment_information(migration_components)
+    update_deploy_jsons(network, deploy_data)
 
 
 if __name__ == "__main__":
