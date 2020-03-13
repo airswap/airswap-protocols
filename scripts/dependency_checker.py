a569cd103a9ccb23d7206351a800c0ec0b442e85
Ethan Wessel <ethan.wessel@fluidity.io>
2020-03-13 12:02:54 -0400

    simplify other areas of code

--- scripts/dependency_checker.py
+++ scripts/dependency_checker.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from collections import defaultdict
 import json
 import sys
 import os
@@ -41,17 +42,18 @@ class DependencyChecker:
                     if DEP in data:
                         self.dependency_graph[package_name][DEP] = data[DEP]
 
-    def identify_and_fix_violations(self, fix):
+    def contains_packages(self, dep):
+        for package_type in PACKAGE_TYPES:
+            if package_type in dep:
+                return True
+        return False
 
-        # flag to determine if return is 0 or 1
-        is_stable = True
+    def identify_violations(self):
+        violation_needed_version = defaultdict(lambda: defaultdict(defaultdict))
         print()
 
         # go through every package looking for where the dependency doesn't match the dependency graph
-        for package in self.dependency_graph.items():
-            package_name = package[0]
-            package_dependencies = package[1]
-
+        for package_name, package_dependencies in self.dependency_graph.items():
             for dep_type in [DEP, DEV_DEP]:
 
                 # if the package doesn't use a dependency type skip over it
@@ -59,27 +61,42 @@ class DependencyChecker:
                     continue
 
                 # go through all the declared dependencies in a package
-                for declared_name, declared_ver in package_dependencies[dep_type].items():
+                for declared_name, declared_version in package_dependencies[dep_type].items():
                     # skip if we don't see the packages we care about
                     if not self.contains_packages(declared_name):
                         continue
 
                     # check version against declared version
                     expected_version = self.dependency_graph[declared_name]['version']
-                    declared_version = declared_ver
                     if declared_version != expected_version:
-                        if fix:
-                            package_dependencies[dep_type][declared_name] = expected_version
-                        is_stable = False
+                        violation_needed_version[package_name][dep_type][declared_name] = expected_version
                         print("%s%s%s (%s): %s@%s →%s Update to %s %s" % (bcolors.BOLD, package_name, bcolors.ENDC, dep_type, declared_name, declared_version, bcolors.FAIL, expected_version, bcolors.ENDC))
         print()
-        return is_stable
+        return violation_needed_version
 
-    def contains_packages(self, dep):
-        for package_type in PACKAGE_TYPES:
-            if package_type in dep:
-                return True
-        return False
+    def write_fixes(self):
+        for directory in SEARCH_DIR:
+            for filename in Path(os.getcwd() + directory).rglob('package.json'):
+                #ignore node_modules
+                if "node_modules" in str(filename):
+                    continue
+
+                with open(str(filename)) as f:
+                    data = json.load(f)
+
+
+
+                    # # extract metadata
+                    package_name = data['name']
+
+                    # self.dependency_graph[package_name] = {}
+                    # self.dependency_graph[package_name]['version'] = data['version']
+                    #
+                    # # set up the dependencies
+                    # if DEV_DEP in data:
+                    #     self.dependency_graph[package_name][DEV_DEP] = data[DEV_DEP]
+                    # if DEP in data:
+                    #     self.dependency_graph[package_name][DEP] = data[DEP]
 
 
 if __name__ == "__main__":
@@ -91,11 +108,10 @@ if __name__ == "__main__":
 
     checker = DependencyChecker()
     checker.generate_graph()
-    stable = checker.identify_and_fix_violations(args.fix)
-    if not stable and args.fix:
-        # write the violations that have been fixed
-        print('would have written')
+    violation_updates = checker.identify_violations()
+    if not violation_updates and args.fix:
+        checker.write_fixes()
 
-    if not stable:
+    if not violation_updates:
         sys.exit(1)
 
