aa06afc2d863efd1bedc1ae13fbc6b18ed0b2e27
Ethan Wessel <ejwessel@gmail.com>
2020-03-23 15:33:33 -0400

    validator checkWrappedSwap

--- tools/protocols/test/Validator.test.ts
+++ tools/protocols/test/Validator.test.ts
@@ -3,20 +3,62 @@ import { expect } from 'chai'
 import { ethers } from 'ethers'
 import { bigNumberify } from 'ethers/utils'
 import { ADDRESS_ZERO } from '@airswap/constants'
+import { functions } from '@airswap/test-utils'
+import { createOrder, signOrder } from '@airswap/utils'
 
 import { Validator } from '..'
 
-class MockContract {
-  public getLocators(signerToken, senderToken, protocol, limit, cursor) {
-    return {
-      locators: ['locator1', 'locator2'],
-      scores: [100, 10],
-      nextCursor: ADDRESS_ZERO,
-    }
+class MockTransaction {
+  public hash: string
+
+  public constructor() {
+    this.hash = 'trxHash'
+  }
+
+  public wait(confirmations) {
+    return
   }
 }
 
+class MockContract {
+  public checkSwap(order) {
+    return [
+      bigNumberify(1),
+      ['0x746573744572726f720000000000000000000000000000000000000000000000'],
+    ] //ethers.utils.formatBytes32String('testError')
+  }
+
+  public checkWrappedSwap(order, delegateAddress, wrapperAddress) {
+    return [
+      bigNumberify(1),
+      ['0x746573744572726f720000000000000000000000000000000000000000000000'],
+    ] //ethers.utils.formatBytes32String('testError')
+  }
+}
+
+let signer
+let order
+
 describe('Validator', () => {
+  before(async () => {
+    signer = functions.getTestWallet()
+    order = await signOrder(
+      createOrder({
+        signer: {
+          wallet: '',
+          token: '',
+          amount: 0,
+        },
+        sender: {
+          wallet: '',
+          token: '',
+          amount: 0,
+        },
+      }),
+      signer,
+      ''
+    )
+  })
   fancy
     .stub(ethers, 'Contract', () => new MockContract())
     .it('Validator getAddress()', async () => {
@@ -28,5 +70,17 @@ describe('Validator', () => {
     .it('Validator getReason()', async () => {
       const reason = await Validator.getReason('test reason')
       expect(reason).to.equal('test reason')
+    })
+  fancy
+    .stub(ethers, 'Contract', () => new MockContract())
+    .it('Validator checkSwap()', async () => {
+      const errors = await new Validator().checkSwap(order)
+      expect(errors[0]).to.equal('testError')
+    })
+  fancy
+    .stub(ethers, 'Contract', () => new MockContract())
+    .it('Validator checkWrappedSwap()', async () => {
+      const errors = await new Validator().checkWrappedSwap(order, '', '')
+      expect(errors[0]).to.equal('testError')
     })
 })
