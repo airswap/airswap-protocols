cc2fc0dedacb382dedb7449849c19cc99ec90f3c
Ethan Wessel <ejwessel@gmail.com>
2020-03-23 11:21:23 -0400

    pretty:fix

--- tools/protocols/test/Delegate.test.ts
+++ tools/protocols/test/Delegate.test.ts
@@ -22,6 +22,14 @@ class MockContract {
     return 999
   }
 
+  public getSenderSideQuote(signerAmount, signerToken, senderToken) {
+    return 333
+  }
+
+  public provideOrder(order) {
+    return 'trxHash'
+  }
+
   public balanceOf() {
     return bigNumberify(1000)
   }
@@ -61,5 +69,45 @@ describe('Delegate', () => {
       )
       expect(quote.sender.amount).to.equal('333')
       expect(quote.signer.amount).to.equal('999')
+    })
+
+  fancy
+    .stub(ethers, 'Contract', () => new MockContract())
+    .it('Delegate getSenderSideQuote()', async () => {
+      const quote = await new Delegate(ADDRESS_ZERO).getSenderSideQuote(
+        '999',
+        '',
+        ''
+      )
+      expect(quote.protocol).to.equal('0x0001')
+      expect(quote.locator).to.equal(
+        '0x0000000000000000000000000000000000000000'
+      )
+      expect(quote.sender.amount).to.equal('333')
+      expect(quote.signer.amount).to.equal('999')
+    })
+
+  fancy
+    .stub(ethers, 'Contract', () => new MockContract())
+    .it('Delegate provideOrder()', async () => {
+      //      const order = await signOrder(
+      //        createOrder({
+      //          signer: {
+      //            wallet: '',
+      //            token: '',
+      //            amount: 0,
+      //          },
+      //          sender: {
+      //            wallet: '',
+      //            token: '',
+      //            amount: 0,
+      //          },
+      //        }),
+      //        ,
+      //        ''
+      //      )
+      //      console.log(order)
+      //      const trx = await new Delegate(ADDRESS_ZERO).provideOrder(order)
+      //      console.log(trx)
     })
 })
