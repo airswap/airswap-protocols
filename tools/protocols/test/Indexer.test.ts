c270fffa99fc2da0d0471b2fe3ab64da82e31342
Ethan Wessel <ejwessel@gmail.com>
2020-03-23 14:48:15 -0400

    add indexer tests against various protocols

--- tools/protocols/test/Indexer.test.ts
+++ tools/protocols/test/Indexer.test.ts
@@ -2,14 +2,17 @@ import { fancy } from 'fancy-test'
 import { expect } from 'chai'
 import { ethers } from 'ethers'
 import { bigNumberify } from 'ethers/utils'
-import { ADDRESS_ZERO } from '@airswap/constants'
+import { ADDRESS_ZERO, protocols } from '@airswap/constants'
 
 import { Indexer } from '..'
 
 class MockContract {
   public getLocators(signerToken, senderToken, protocol, limit, cursor) {
     return {
-      locators: ['locator1', 'locator2'],
+      locators: [
+        '0x6c6f6361746f7231000000000000000000000000000000000000000000000000', // ethers.utils.formatBytes32String('locator1')
+        '0x6c6f6361746f7232000000000000000000000000000000000000000000000000', // ethers.utils.formatBytes32String('locator2')
+      ],
       scores: [100, 10],
       nextCursor: ADDRESS_ZERO,
     }
@@ -25,10 +28,50 @@ describe('Indexer', () => {
     })
   fancy
     .stub(ethers, 'Contract', () => new MockContract())
-    .it('Indexer getLocators()', async () => {
-      const result = await new Indexer().getLocators('', '', '', 0, '')
+    .it('Indexer SERVER getLocators()', async () => {
+      const result = await new Indexer().getLocators(
+        '',
+        '',
+        protocols.SERVER,
+        0,
+        ''
+      )
       expect(result.locators[0]).to.equal('locator1')
       expect(result.locators[1]).to.equal('locator2')
+      expect(result.scores[0]).to.equal(100)
+      expect(result.scores[1]).to.equal(10)
+      expect(result.nextCursor).to.equal(ADDRESS_ZERO)
+    })
+  fancy
+    .stub(ethers, 'Contract', () => new MockContract())
+    .it('Indexer DELEGATE getLocators()', async () => {
+      const result = await new Indexer().getLocators(
+        '',
+        '',
+        protocols.DELEGATE,
+        0,
+        ''
+      )
+      expect(result.locators[0]).to.equal(
+        '0x6c6f6361746F7231000000000000000000000000'
+      )
+      expect(result.locators[1]).to.equal(
+        '0x6C6f6361746F7232000000000000000000000000'
+      )
+      expect(result.scores[0]).to.equal(100)
+      expect(result.scores[1]).to.equal(10)
+      expect(result.nextCursor).to.equal(ADDRESS_ZERO)
+    })
+  fancy
+    .stub(ethers, 'Contract', () => new MockContract())
+    .it('Indexer DEFAULT getLocators()', async () => {
+      const result = await new Indexer().getLocators('', '', 'DEFAULT', 0, '')
+      expect(result.locators[0]).to.equal(
+        '0x6c6f6361746f7231000000000000000000000000000000000000000000000000'
+      )
+      expect(result.locators[1]).to.equal(
+        '0x6c6f6361746f7232000000000000000000000000000000000000000000000000'
+      )
       expect(result.scores[0]).to.equal(100)
       expect(result.scores[1]).to.equal(10)
       expect(result.nextCursor).to.equal(ADDRESS_ZERO)
