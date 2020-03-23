2425b0db46b9ece602af2ed0d157ad159e9bb29c
Ethan Wessel <ejwessel@gmail.com>
2020-03-23 17:21:26 -0400

    Delegate throw tests

--- tools/protocols/test/Delegate.test.ts
+++ tools/protocols/test/Delegate.test.ts
@@ -49,14 +49,60 @@ class MockContract {
   }
 }
 
+class MockContractBAD {
+  public getMaxQuote(senderToken, signerToken) {
+    return {
+      senderAmount: 0,
+      signerAmount: 0,
+    }
+  }
+
+  public getSignerSideQuote(senderAmount, senderToken, signerToken) {
+    return 0
+  }
+
+  public getSenderSideQuote(signerAmount, signerToken, senderToken) {
+    return 0
+  }
+
+  public provideOrder(order) {
+    return new MockTransaction()
+  }
+
+  public balanceOf() {
+    return bigNumberify(0)
+  }
+}
+
+let signer
+let order
+
 describe('Delegate', () => {
+  before(async () => {
+    signer = functions.getTestWallet()
+    order = await signOrder(
+      createOrder({
+        signer: {
+          wallet: '',
+          token: '',
+          amount: 0,
+        },
+        sender: {
+          wallet: '',
+          token: '',
+          amount: 0,
+        },
+      }),
+      signer,
+      ''
+    )
+  })
   fancy
     .stub(ethers, 'Contract', () => new MockContract())
     .it('Delegate getWallet()', async () => {
       const wallet = await new Delegate(ADDRESS_ZERO).getWallet()
       expect(wallet).to.equal('tradeWallet')
     })
-
   fancy
     .stub(ethers, 'Contract', () => new MockContract())
     .it('Delegate getMaxQuote()', async () => {
@@ -66,7 +112,13 @@ describe('Delegate', () => {
       expect(quote.sender.amount).to.equal('333')
       expect(quote.signer.amount).to.equal('999')
     })
-
+  fancy
+    .stub(ethers, 'Contract', () => new MockContractBAD())
+    .do(async () => {
+      await new Delegate(ADDRESS_ZERO).getMaxQuote('', '')
+    })
+    .catch(/Not quoting for requested amount or token pair/)
+    .it('Delegate getMaxQuote() throw')
   fancy
     .stub(ethers, 'Contract', () => new MockContract())
     .it('Delegate getSignerSideQuote()', async () => {
@@ -80,7 +132,13 @@ describe('Delegate', () => {
       expect(quote.sender.amount).to.equal('333')
       expect(quote.signer.amount).to.equal('999')
     })
-
+  fancy
+    .stub(ethers, 'Contract', () => new MockContractBAD())
+    .do(async () => {
+      await new Delegate(ADDRESS_ZERO).getSignerSideQuote('', '', '')
+    })
+    .catch(/Not quoting for requested amount or token pair/)
+    .it('Delegate getSignerSideQuote() throw')
   fancy
     .stub(ethers, 'Contract', () => new MockContract())
     .it('Delegate getSenderSideQuote()', async () => {
@@ -94,28 +152,25 @@ describe('Delegate', () => {
       expect(quote.sender.amount).to.equal('333')
       expect(quote.signer.amount).to.equal('999')
     })
-
+  fancy
+    .stub(ethers, 'Contract', () => new MockContractBAD())
+    .do(async () => {
+      await new Delegate(ADDRESS_ZERO).getSenderSideQuote('', '', '')
+    })
+    .catch(/Not quoting for requested amount or token pair/)
+    .it('Delegate getSenderSideQuote() throw')
   fancy
     .stub(ethers, 'Contract', () => new MockContract())
     .it('Delegate provideOrder()', async () => {
-      const signer = functions.getTestWallet()
-      const order = await signOrder(
-        createOrder({
-          signer: {
-            wallet: '',
-            token: '',
-            amount: 0,
-          },
-          sender: {
-            wallet: '',
-            token: '',
-            amount: 0,
-          },
-        }),
-        signer,
-        ''
-      )
       const trx = await new Delegate(ADDRESS_ZERO).provideOrder(order, signer)
       expect(trx).to.equal('trxHash')
     })
+  fancy
+    .stub(ethers, 'Contract', () => new MockContract())
+    .do(async () => {
+      const trx = await new Delegate(ADDRESS_ZERO).provideOrder(order)
+      expect(trx).to.equal('trxHash')
+    })
+    .catch(/Wallet must be provided/)
+    .it('Delegate provideOrder() throw')
 })
