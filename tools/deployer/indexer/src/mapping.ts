a469ab300ef888add65184ba9de5fbad38ab3729
Ethan Wessel <ethan.wessel@fluidity.io>
2020-04-06 16:54:11 -0400

    mapping for creation of index and blacklist

--- tools/deployer/indexer/src/mapping.ts
+++ tools/deployer/indexer/src/mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, log, EthereumEventParam } from "@graphprotocol/graph-ts"
 import {
   Contract,
   AddTokenToBlacklist,
@@ -8,7 +8,7 @@ import {
   Stake,
   Unstake
 } from "../generated/Contract/Contract"
-import { Token } from "../generated/schema"
+import { Token, Index } from "../generated/schema"
 
 export function handleAddTokenToBlacklist(event: AddTokenToBlacklist): void {
   let token = Token.load(event.params.token.toHex())
@@ -21,9 +21,36 @@ export function handleAddTokenToBlacklist(event: AddTokenToBlacklist): void {
   token.save()
 }
 
-export function handleCreateIndex(event: CreateIndex): void {}
+export function handleCreateIndex(event: CreateIndex): void {
+  // 0x0000000000000000000000000000000000000000 + 0x0000
+  // -> 0x00000000000000000000000000000000000000000x0000
+  let indexIdentifier = event.params.indexAddress.toHex() + event.params.protocol.toHex()
+  let index = new Index(indexIdentifier)
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+  // handle creation of signer tokens if it doesn't exist
+  let signerToken = Token.load(event.params.signerToken.toHex())
+  if (signerToken == null) {
+    signerToken = new Token(event.params.signerToken.toHex())
+    signerToken.isBlacklisted = false
+    signerToken.save()
+  }
+
+  // handle creation of sender tokens if it doesn't exist
+  let senderToken = Token.load(event.params.senderToken.toHex())
+  if (senderToken == null) {
+    senderToken = new Token(event.params.senderToken.toHex())
+    senderToken.isBlacklisted = false
+    senderToken.save()
+  }
+  
+  index.signerToken = signerToken.id
+  index.senderToken = senderToken.id
+  index.save()
+}
+
+export function handleOwnershipTransferred(event: OwnershipTransferred): void {
+  /* Not Implemented or Tracked */
+}
 
 export function handleRemoveTokenFromBlacklist(
   event: RemoveTokenFromBlacklist
