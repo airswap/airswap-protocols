52b40d13f4e899acc0380ace16591da241b162cf
Ethan Wessel <ethan.wessel@fluidity.io>
2020-04-08 08:56:31 -0400

    update setup

---
+++ tools/deployer/delegate/src/DelegateFactoryMapping.ts
@@ -0,0 +1,6 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import { Contract, CreateDelegate } from "../generated/Contract/Contract"
+import { ExampleEntity } from "../generated/schema"
+
+export function handleCreateDelegate(event: CreateDelegate): void {
+}
