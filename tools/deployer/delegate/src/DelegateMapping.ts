4c8793b85f59a66c114f22e22fe12a61d05ac39f
Ethan Wessel <ejwessel@gmail.com>
2020-04-08 15:54:37 -0400

    delegate mapping

--- tools/deployer/delegate/src/DelegateMapping.ts
+++ tools/deployer/delegate/src/DelegateMapping.ts
@@ -3,6 +3,22 @@ import { ProvideOrder, SetRule, UnsetRule } from "../generated/templates/Delegat
 import { DelegateRule, ProvidedOrder } from "../generated/schema"
 
 export function handleSetRule(event: SetRule): void {
+  var ruleIdentifier = 
+    event.params.owner.toHex() + 
+    event.params.senderToken.toHex() + 
+    event.params.signerToken.toHex()
+
+  var rule = DelegateRule.load(ruleIdentifier)
+  if (!rule) {
+    rule = new DelegateRule(ruleIdentifier)
+    rule.owner = event.params.owner
+    rule.signerToken = event.params.signerToken
+    rule.senderToken = event.params.senderToken
+  }
+  rule.maxSenderAmount = event.params.maxSenderAmount
+  rule.priceCoef = event.params.priceCoef
+  rule.priceExp = event.params.priceExp
+  rule.save()
 }
 
 export function handleUnsetRule(event: UnsetRule): void {
