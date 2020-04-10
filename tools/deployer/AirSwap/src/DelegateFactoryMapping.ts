29cf329f0fa2ceb022f24db704cfbabf6b502741
Ethan Wessel <ethan.wessel@fluidity.io>
2020-04-10 16:17:58 -0400

    update delegate factory mapping

--- tools/deployer/AirSwap/src/DelegateFactoryMapping.ts
+++ tools/deployer/AirSwap/src/DelegateFactoryMapping.ts
@@ -1,10 +1,11 @@
-import { log } from "@graphprotocol/graph-ts"
+import { BigInt, log } from "@graphprotocol/graph-ts"
 import { CreateDelegate } from "../generated/DelegateFactory/DelegateFactory"
 import { Delegate } from '../generated/templates'
 import { User, Indexer, SwapContract, DelegateFactory, DelegateContract } from "../generated/schema"
 
 export function handleCreateDelegate(event: CreateDelegate): void {
-
+  log.info("DELEGATE CREATED!",[])
+  
   // handle delegate factory if it doesn't exist
   var delegateFactory = DelegateFactory.load(event.address.toHex())
   if (!delegateFactory) {
@@ -12,12 +13,37 @@ export function handleCreateDelegate(event: CreateDelegate): void {
     delegateFactory.save()
   }
 
+  // handle swap contract if it doesn't exist
+  var swap = SwapContract.load(event.params.swapContract.toHex())
+  if (!swap) {
+    swap = new SwapContract(event.params.swapContract.toHex())
+    swap.save()
+  }
+
+  // handle indexer if it doesn't exist
+  var indexer = Indexer.load(event.params.indexerContract.toHex())
+  if (!indexer) {
+    indexer = new Indexer(event.params.indexerContract.toHex())
+    indexer.save()
+  }
+
+  // handle user if it doesn't exist
+  var owner = User.load(event.params.delegateContractOwner.toHex())
+  if (!owner) {
+    owner = new User(event.params.delegateContractOwner.toHex())
+    owner.authorizedSigners = new Array<string>()
+    owner.authorizedSenders = new Array<string>()
+    owner.executedOrders = new Array<string>()
+    owner.cancelledNonces = new Array<BigInt>()
+    owner.save()
+  }
+
   Delegate.create(event.params.delegateContract)
   var delegate = new DelegateContract(event.params.delegateContract.toHex())
   delegate.factory = delegateFactory.id
-  delegate.swap = SwapContract.load(event.params.swapContract.toHex()).id
-  delegate.indexer = Indexer.load(event.params.indexerContract.toHex()).id
-  delegate.owner = User.load(event.params.delegateContractOwner.toHex()).id
+  delegate.swap = swap.id
+  delegate.indexer = indexer.id
+  delegate.owner = owner.id
   delegate.tradeWallet = event.params.delegateTradeWallet
   delegate.save()
 }
