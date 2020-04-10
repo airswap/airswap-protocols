dde839b58cd893bda16c1aeb0cea0f803256d9ee
Ethan Wessel <ethan.wessel@fluidity.io>
2020-04-10 16:44:18 -0400

    update delegate mapping

--- tools/deployer/AirSwap/src/DelegateMapping.ts
+++ tools/deployer/AirSwap/src/DelegateMapping.ts
@@ -3,6 +3,31 @@ import { ProvideOrder, SetRule, UnsetRule } from "../generated/templates/Delegat
 import { User, Token, DelegateContract, Rule } from "../generated/schema"
 
 export function handleSetRule(event: SetRule): void {
+  // handle user if it doesn't exist
+  var owner = User.load(event.params.owner.toHex())
+  if (!owner) {
+    owner = new User(event.params.owner.toHex())
+    owner.authorizedSigners = new Array<string>()
+    owner.authorizedSenders = new Array<string>()
+    owner.executedOrders = new Array<string>()
+    owner.cancelledNonces = new Array<BigInt>()
+    owner.save()
+  }
+
+  var signerToken = Token.load(event.params.signerToken.toHex())
+  if (!signerToken) {
+    signerToken = new Token(event.params.signerToken.toHex())
+    signerToken.isBlacklisted = false
+    signerToken.save()
+  }
+
+  var senderToken = Token.load(event.params.senderToken.toHex())
+  if (!senderToken) {
+    senderToken = new Token(event.params.senderToken.toHex())
+    senderToken.isBlacklisted = false
+    senderToken.save()
+  }
+
   var ruleIdentifier = 
     event.address.toHex() + 
     event.params.senderToken.toHex() + 
@@ -12,10 +37,10 @@ export function handleSetRule(event: SetRule): void {
   // create base portion of rule if it doesn't not exist
   if (!rule) {
     rule = new Rule(ruleIdentifier)
-    rule.delegate = DelegateContract.load(event.address.toHexString()).id
-    rule.owner = User.load(event.params.owner.toHex()).id
-    rule.signerToken = Token.load(event.params.signerToken.toHex()).id
-    rule.senderToken = Token.load(event.params.senderToken.toHex()).id
+    rule.delegate = DelegateContract.load(event.address.toHex()).id
+    rule.owner = owner.id
+    rule.signerToken = signerToken.id
+    rule.senderToken = senderToken.id
   }
   rule.maxSenderAmount = event.params.maxSenderAmount
   rule.priceCoef = event.params.priceCoef
@@ -33,17 +58,17 @@ export function handleUnsetRule(event: UnsetRule): void {
 }
 
 export function handleProvideOrder(event: ProvideOrder): void {
-  var ruleIdentifier = 
-    event.address.toHex() + 
-    event.params.senderToken.toHex() + 
-    event.params.signerToken.toHex()
+  // var ruleIdentifier = 
+  //   event.address.toHex() + 
+  //   event.params.senderToken.toHex() + 
+  //   event.params.signerToken.toHex()
 
-  var rule = Rule.load(ruleIdentifier)
-  rule.maxSenderAmount = BigInt.fromI32(rule.maxSenderAmount.toI32() - event.params.senderAmount.toI32())
-  // if rule is to have been fully consumed, remove it
-  if (rule.maxSenderAmount == BigInt.fromI32(0)) {
-    store.remove("Rule", ruleIdentifier)
-  } else {
-    rule.save()
-  }
+  // var rule = Rule.load(ruleIdentifier)
+  // rule.maxSenderAmount = BigInt.fromI32(rule.maxSenderAmount.toI32() - event.params.senderAmount.toI32())
+  // // if rule is to have been fully consumed, remove it
+  // if (rule.maxSenderAmount == BigInt.fromI32(0)) {
+  //   store.remove("Rule", ruleIdentifier)
+  // } else {
+  //   rule.save()
+  // }
 }
