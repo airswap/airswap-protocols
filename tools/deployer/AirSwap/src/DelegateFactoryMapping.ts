7dd7c186c4d684cea79838ad574827349c5c579f
Ethan Wessel <ethan.wessel@fluidity.io>
2020-04-13 10:18:58 -0400

    let to var

--- tools/deployer/AirSwap/src/DelegateFactoryMapping.ts
+++ tools/deployer/AirSwap/src/DelegateFactoryMapping.ts
@@ -7,28 +7,28 @@ export function handleCreateDelegate(event: CreateDelegate): void {
   log.info("DELEGATE CREATED!",[])
   
   // handle delegate factory if it doesn't exist
-  var delegateFactory = DelegateFactory.load(event.address.toHex())
+  let delegateFactory = DelegateFactory.load(event.address.toHex())
   if (!delegateFactory) {
     delegateFactory = new DelegateFactory(event.address.toHex())
     delegateFactory.save()
   }
 
   // handle swap contract if it doesn't exist
-  var swap = SwapContract.load(event.params.swapContract.toHex())
+  let swap = SwapContract.load(event.params.swapContract.toHex())
   if (!swap) {
     swap = new SwapContract(event.params.swapContract.toHex())
     swap.save()
   }
 
   // handle indexer if it doesn't exist
-  var indexer = Indexer.load(event.params.indexerContract.toHex())
+  let indexer = Indexer.load(event.params.indexerContract.toHex())
   if (!indexer) {
     indexer = new Indexer(event.params.indexerContract.toHex())
     indexer.save()
   }
 
   // handle user if it doesn't exist
-  var owner = User.load(event.params.delegateContractOwner.toHex())
+  let owner = User.load(event.params.delegateContractOwner.toHex())
   if (!owner) {
     owner = new User(event.params.delegateContractOwner.toHex())
     owner.authorizedSigners = new Array<string>()
@@ -39,7 +39,7 @@ export function handleCreateDelegate(event: CreateDelegate): void {
   }
 
   Delegate.create(event.params.delegateContract)
-  var delegate = new DelegateContract(event.params.delegateContract.toHex())
+  let delegate = new DelegateContract(event.params.delegateContract.toHex())
   delegate.factory = delegateFactory.id
   delegate.swap = swap.id
   delegate.indexer = indexer.id
