bd658e114a180edb317577450db95e45db9e801c
Ethan Wessel <ejwessel@gmail.com>
2020-06-23 16:02:34 -0700

    Deploy new indices

--- source/indexer/migrations/1_initial_migration.js
+++ source/indexer/migrations/1_initial_migration.js
@@ -1,9 +1,9 @@
 const Indexer = artifacts.require('Indexer')
 
 module.exports = (deployer, network) => {
-  if (network == 'rinkeby' || network == 'mainnet') {
-    // fill in the address of this contract
-    const STAKING_TOKEN_ADDRESS = ''
-    deployer.deploy(Indexer, STAKING_TOKEN_ADDRESS)
-  }
+  // if (network == 'rinkeby' || network == 'mainnet') {
+  //   // fill in the address of this contract
+  //   const STAKING_TOKEN_ADDRESS = ''
+  //   deployer.deploy(Indexer, STAKING_TOKEN_ADDRESS)
+  // }
 }
