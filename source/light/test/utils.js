1a03a3f2ccc08cfcc4f5e89bc9921a85f621e0b8
Vicente Dragicevic <vdrg@protonmail.ch>
2021-01-05 16:53:53 -0300

    Move EIP712 domain to types and remove unused functions from utils

--- source/light/test/utils.js
+++ source/light/test/utils.js
@@ -4,6 +4,8 @@ const ethers = require('ethers')
 const bip39 = require('bip39')
 const hdkey = require('ethereumjs-wallet/hdkey')
 
+const { EIP712Light } = require('@airswap/types')
+
 // We assume deterministic ganache mnemonic is being used
 const GANACHE_MNEMONIC =
   'myth like bonus scare over problem client lizard pioneer submit female collect'
@@ -12,107 +14,14 @@ const HD_PATH = "m/44'/60'/0'/0/"
 
 const hdwallet = hdkey.fromMasterSeed(bip39.mnemonicToSeed(GANACHE_MNEMONIC))
 
-const EIP712 = {
-  EIP712Domain: [
-    { name: 'name', type: 'string' },
-    { name: 'version', type: 'string' },
-    { name: 'chainId', type: 'uint256' },
-    { name: 'verifyingContract', type: 'address' },
-  ],
-  LightOrder: [
-    { name: 'nonce', type: 'uint256' },
-    { name: 'expiry', type: 'uint256' },
-    { name: 'sender', type: 'address' },
-    { name: 'signerToken', type: 'address' },
-    { name: 'signerAmount', type: 'uint256' },
-    { name: 'senderToken', type: 'address' },
-    { name: 'senderAmount', type: 'uint256' },
-  ],
-}
-
-function stringify(type) {
-  let str = `${type}(`
-  const keys = Object.keys(EIP712[type])
-  for (let i = 0; i < keys.length; i++) {
-    str += `${EIP712[type][i].type} ${EIP712[type][i].name}`
-    if (i !== keys.length - 1) {
-      str += ','
-    }
-  }
-  return `${str})`
-}
-
-const ORDER_TYPEHASH = ethUtil.keccak256(stringify('LightOrder'))
-
-const EIP712_DOMAIN_TYPEHASH = ethUtil.keccak256(stringify('EIP712Domain'))
-
 const DOMAIN_NAME = 'SWAP_LIGHT'
 const DOMAIN_VERSION = '3'
 const DOMAIN_CHAIN_ID = 1
 
-function hashOrder(order) {
-  return ethUtil.keccak256(
-    ethers.utils.defaultAbiCoder.encode(
-      [
-        'bytes32',
-        'uint256',
-        'uint256',
-        'address',
-        'address',
-        'uint256',
-        'address',
-        'uint256',
-      ],
-      [
-        ORDER_TYPEHASH,
-        order.nonce,
-        order.expiry,
-        order.sender,
-        order.signerToken,
-        order.signerAmount,
-        order.senderToken,
-        order.senderAmount,
-      ]
-    )
-  )
-}
-
-function hashDomain(swapContract) {
-  return ethUtil.keccak256(
-    ethers.utils.defaultAbiCoder.encode(
-      ['bytes32', 'bytes32', 'bytes32', 'uint256', 'address'],
-      [
-        EIP712_DOMAIN_TYPEHASH,
-        ethUtil.keccak256(DOMAIN_NAME),
-        ethUtil.keccak256(DOMAIN_VERSION),
-        DOMAIN_CHAIN_ID,
-        swapContract,
-      ]
-    )
-  )
-}
-
-function getOrderHash(order, swapContract) {
-  return ethUtil.keccak256(
-    Buffer.concat([
-      Buffer.from('1901', 'hex'),
-      hashDomain(swapContract),
-      hashOrder(order),
-    ])
-  )
-}
-
-async function createSignature(order, wallet, swapContract) {
-  const orderHash = getOrderHash(order, swapContract)
-  const sig = await wallet.signMessage(ethers.utils.arrayify(orderHash))
-  const v = ethers.utils.splitSignature(sig).v
-  return `${sig.slice(0, 130)}${(v === 0 || v === 1 ? v + 27 : v).toString(16)}`
-}
-
 function getTypedDataSignature(order, privateKey, verifyingContract) {
   const sig = sigUtil.signTypedData_v4(ethUtil.toBuffer(privateKey), {
     data: {
-      types: EIP712,
+      types: EIP712Light,
       domain: {
         name: DOMAIN_NAME,
         version: DOMAIN_VERSION,
@@ -155,7 +64,5 @@ function signOrder(order, account, swapContract) {
 
 module.exports = {
   getPrivateKeyFromGanacheAccount,
-  getOrderHash,
-  createSignature,
   signOrder,
 }
