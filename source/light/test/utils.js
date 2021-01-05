883e5df7f8a9bc9896c67e02919de4b5e38547b4
Vicente Dragicevic <vdrg@protonmail.ch>
2021-01-05 14:05:14 -0300

    Use signTypedData

--- source/light/test/utils.js
+++ source/light/test/utils.js
@@ -1,5 +1,16 @@
-const util = require('ethereumjs-util')
-const { ethers } = require('ethers')
+const ethUtil = require('ethereumjs-util')
+const sigUtil = require('eth-sig-util')
+const ethers = require('ethers')
+const bip39 = require('bip39')
+const hdkey = require('ethereumjs-wallet/hdkey')
+
+// We assume deterministic ganache mnemonic is being used
+const GANACHE_MNEMONIC =
+  'myth like bonus scare over problem client lizard pioneer submit female collect'
+
+const HD_PATH = "m/44'/60'/0'/0/"
+
+const hdwallet = hdkey.fromMasterSeed(bip39.mnemonicToSeed(GANACHE_MNEMONIC))
 
 const EIP712 = {
   EIP712Domain: [
@@ -31,16 +42,16 @@ function stringify(type) {
   return `${str})`
 }
 
-const ORDER_TYPEHASH = util.keccak256(stringify('LightOrder'))
+const ORDER_TYPEHASH = ethUtil.keccak256(stringify('LightOrder'))
 
-const EIP712_DOMAIN_TYPEHASH = util.keccak256(stringify('EIP712Domain'))
+const EIP712_DOMAIN_TYPEHASH = ethUtil.keccak256(stringify('EIP712Domain'))
 
 const DOMAIN_NAME = 'SWAP_LIGHT'
 const DOMAIN_VERSION = '3'
 const DOMAIN_CHAIN_ID = 1
 
 function hashOrder(order) {
-  return util.keccak256(
+  return ethUtil.keccak256(
     ethers.utils.defaultAbiCoder.encode(
       [
         'bytes32',
@@ -67,13 +78,13 @@ function hashOrder(order) {
 }
 
 function hashDomain(swapContract) {
-  return util.keccak256(
+  return ethUtil.keccak256(
     ethers.utils.defaultAbiCoder.encode(
       ['bytes32', 'bytes32', 'bytes32', 'uint256', 'address'],
       [
         EIP712_DOMAIN_TYPEHASH,
-        util.keccak256(DOMAIN_NAME),
-        util.keccak256(DOMAIN_VERSION),
+        ethUtil.keccak256(DOMAIN_NAME),
+        ethUtil.keccak256(DOMAIN_VERSION),
         DOMAIN_CHAIN_ID,
         swapContract,
       ]
@@ -82,7 +93,7 @@ function hashDomain(swapContract) {
 }
 
 function getOrderHash(order, swapContract) {
-  return util.keccak256(
+  return ethUtil.keccak256(
     Buffer.concat([
       Buffer.from('1901', 'hex'),
       hashDomain(swapContract),
@@ -98,14 +109,52 @@ async function createSignature(order, wallet, swapContract) {
   return `${sig.slice(0, 130)}${(v === 0 || v === 1 ? v + 27 : v).toString(16)}`
 }
 
-async function signOrder(order, wallet, swapContract) {
+function getTypedDataSignature(order, privateKey, verifyingContract) {
+  const sig = sigUtil.signTypedData_v4(ethUtil.toBuffer(privateKey), {
+    data: {
+      types: EIP712,
+      domain: {
+        name: DOMAIN_NAME,
+        version: DOMAIN_VERSION,
+        chainId: DOMAIN_CHAIN_ID,
+        verifyingContract,
+      },
+      primaryType: 'LightOrder',
+      message: order,
+    },
+  })
+
+  const v = ethers.utils.splitSignature(sig).v
+  return `${sig.slice(0, 130)}${(v === 0 || v === 1 ? v + 27 : v).toString(16)}`
+}
+
+function getPrivateKeyFromGanacheAccount(account) {
+  for (let i = 0; i < 100; i++) {
+    const node = hdwallet.derivePath(HD_PATH + String(i))
+    const key = node.getWallet().getPrivateKey()
+    if (
+      ethUtil.bufferToHex(ethUtil.privateToAddress(key)).toLowerCase() ===
+      account.toLowerCase()
+    ) {
+      return ethUtil.bufferToHex(key)
+    }
+  }
+  throw new Error('No private key found for address')
+}
+
+function signOrder(order, account, swapContract) {
   return {
     ...order,
-    signature: await createSignature(order, wallet, swapContract),
+    signature: getTypedDataSignature(
+      order,
+      getPrivateKeyFromGanacheAccount(account),
+      swapContract
+    ),
   }
 }
 
 module.exports = {
+  getPrivateKeyFromGanacheAccount,
   getOrderHash,
   createSignature,
   signOrder,
