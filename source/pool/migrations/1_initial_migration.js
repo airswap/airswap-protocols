b074db57d6faf552889431898926b06c66f78797
Ethan Wessel <ejwessel@gmail.com>
2021-03-04 11:43:30 -0800

    preparation for deployment

--- source/pool/migrations/1_initial_migration.js
+++ source/pool/migrations/1_initial_migration.js
@@ -4,7 +4,7 @@ module.exports = (deployer, network) => {
   if (network == 'rinkeby' || network == 'mainnet') {
     // Fill in these values prior to deploying
     const POOL_SCALE = 10
-    const POOL_MAX = 50
+    const POOL_MAX = 100
     deployer.deploy(Pool, POOL_SCALE, POOL_MAX)
   }
 }
