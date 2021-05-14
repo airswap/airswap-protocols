e21039b50e861c4a84e8981bf117aa3a7d0621f2
Don Mosites <mosites@gmail.com>
2021-05-14 18:27:09 -0400

    Rinkeby deploy for @airswap/registry

--- source/registry/scripts/registry-deploy.js
+++ source/registry/scripts/registry-deploy.js
@@ -8,8 +8,8 @@ async function main() {
   const [deployer] = await ethers.getSigners()
   console.log(`Deployer Address: ${deployer.address}`)
   const stakingToken = '0xcc1cbd4f67cceb7c001bd4adf98451237a193ff8'
-  const obligationCost = 10000000
-  const tokenCost = 100000
+  const obligationCost = 100000
+  const tokenCost = 10000
   const registryFactory = await ethers.getContractFactory('Registry')
   const registryContract = await registryFactory.deploy(
     stakingToken,
