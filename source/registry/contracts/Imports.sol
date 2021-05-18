38d7b57d08a16599b06d31df56131ff20aa62883
Ethan Wessel <ejwessel@gmail.com>
2021-05-18 08:38:43 -0700

    Registry Integration

---
+++ source/registry/contracts/Imports.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.8.0;
+
+// We import the contract so truffle compiles it, and we have the ABI
+// available when working from truffle console.
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
--- source/registry/hardhat.config.js
+++ source/registry/hardhat.config.js
@@ -13,6 +13,12 @@ require('solidity-coverage')
  */
 module.exports = {
   networks: {
+    // forking is always enabled
+    hardhat: {
+      forking: {
+        url: 'https://mainnet.infura.io/v3/' + process.env.INFURA_API_KEY,
+      },
+    },
     localhost: {
       url: 'http://127.0.0.1:8545',
       timeout: 1000000,
