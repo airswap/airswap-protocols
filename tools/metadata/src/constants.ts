850fd637819b47f7fde88d3dcbde8dc904acd602
Don Mosites <mosites@gmail.com>
2021-05-18 12:52:44 -0400

    Token lists for @airswap/metadata, numbers for chainIds

--- tools/metadata/src/constants.ts
+++ tools/metadata/src/constants.ts
@@ -1,23 +1,70 @@
-import { NormalizedToken } from './types'
-import { tokenKinds } from '@airswap/constants'
+export const known = [
+  'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json',
+  'https://umaproject.org/uma.tokenlist.json',
+  'https://raw.githubusercontent.com/SetProtocol/uniswap-tokenlist/main/set.tokenlist.json',
+  'https://raw.githubusercontent.com/opynfinance/opyn-tokenlist/master/opyn-v1.tokenlist.json',
+  'https://app.tryroll.com/tokens.json',
+  'https://tokens.coingecko.com/uniswap/all.json',
+]
 
-export const IDEX_TOKEN_API = 'https://api.idex.io/v1/assets'
-export const TRUST_WALLET_IMAGE_API =
-  'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets'
-
-export const METAMASK_IMAGE_API =
-  'https://raw.githubusercontent.com/MetaMask/eth-contract-metadata/master/images'
-
-export const getOpenSeaUrl = chainId => {
-  switch (Number(chainId)) {
-    case 4:
-      return 'https://rinkeby-api.opensea.io/api/v1'
-    case 1:
-      return 'https://api.opensea.io/api/v1'
-    default:
-      throw new Error('OpenSea only supports rinkeby and mainnet')
-  }
-}
+export const defaults = [
+  {
+    name: 'Wrapped Ether',
+    address: '0xc778417e063141139fce010982780140aa0cd5ab',
+    decimals: 18,
+    symbol: 'WETH',
+    chainId: 4,
+  },
+  {
+    name: 'Wrapped Ether',
+    address: '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6',
+    decimals: 18,
+    symbol: 'WETH',
+    chainId: 5,
+  },
+  {
+    name: 'Wrapped Ether',
+    address: '0xd0a1e359811322d97991e03f863a0c30c2cf029c',
+    decimals: 18,
+    symbol: 'WETH',
+    chainId: 42,
+  },
+  {
+    name: 'Ether',
+    address: '0x2170ed0880ac9a755fd29b2688956bd959f933f8',
+    decimals: 18,
+    symbol: 'ETH',
+    chainId: 56,
+  },
+  {
+    name: 'AirSwap Token',
+    address: '0xcc1cbd4f67cceb7c001bd4adf98451237a193ff8',
+    decimals: 4,
+    symbol: 'AST',
+    chainId: 4,
+  },
+  {
+    name: 'AirSwap Token',
+    address: '0x1a1ec25dc08e98e5e93f1104b5e5cdd298707d31',
+    decimals: 4,
+    symbol: 'AST',
+    chainId: 5,
+  },
+  {
+    name: 'AirSwap Token',
+    address: '0x1a1ec25dc08e98e5e93f1104b5e5cdd298707d31',
+    decimals: 4,
+    symbol: 'AST',
+    chainId: 42,
+  },
+  {
+    name: 'AirSwap Token',
+    address: '0x1ac0d76f11875317f8a7d791db94cdd82bd02bd1',
+    decimals: 4,
+    symbol: 'AST',
+    chainId: 56,
+  },
+]
 
 export const ERC20_BYTES32_ABI = [
   {
@@ -254,71 +301,3 @@ export const ERC20_ABI = [
     type: 'event',
   },
 ]
-
-export const rinkebyTokensByAddress: Record<string, NormalizedToken> = {
-  '0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea': {
-    name: 'Dai Stablecoin',
-    address: '0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea',
-    decimals: 18,
-    symbol: 'DAI',
-    kind: tokenKinds.ERC20,
-  },
-  '0xc778417e063141139fce010982780140aa0cd5ab': {
-    name: 'Wrapped Ether',
-    address: '0xc778417e063141139fce010982780140aa0cd5ab',
-    decimals: 18,
-    symbol: 'WETH',
-    kind: tokenKinds.ERC20,
-  },
-  '0xcc1cbd4f67cceb7c001bd4adf98451237a193ff8': {
-    name: 'AirSwap Token',
-    address: '0xcc1cbd4f67cceb7c001bd4adf98451237a193ff8',
-    decimals: 4,
-    symbol: 'AST',
-    kind: tokenKinds.ERC20,
-  },
-}
-
-export const goerliTokensByAddress: Record<string, NormalizedToken> = {
-  '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6': {
-    name: 'Wrapped Ether',
-    address: '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6',
-    decimals: 18,
-    symbol: 'WETH',
-    kind: tokenKinds.ERC20,
-  },
-  '0x1a1ec25dc08e98e5e93f1104b5e5cdd298707d31': {
-    name: 'AirSwap Token',
-    address: '0x1a1ec25dc08e98e5e93f1104b5e5cdd298707d31',
-    decimals: 4,
-    symbol: 'AST',
-    kind: tokenKinds.ERC20,
-  },
-}
-
-export const kovanTokensByAddress: Record<string, NormalizedToken> = {
-  '0xd0a1e359811322d97991e03f863a0c30c2cf029c': {
-    name: 'Wrapped Ether',
-    address: '0xd0a1e359811322d97991e03f863a0c30c2cf029c',
-    decimals: 18,
-    symbol: 'WETH',
-    kind: tokenKinds.ERC20,
-  },
-  '0x1a1ec25dc08e98e5e93f1104b5e5cdd298707d31': {
-    name: 'AirSwap Token',
-    address: '0x1a1ec25dc08e98e5e93f1104b5e5cdd298707d31',
-    decimals: 4,
-    symbol: 'AST',
-    kind: tokenKinds.ERC20,
-  },
-}
-
-export const binanceTokensByAddress: Record<string, NormalizedToken> = {
-  '0x1ac0d76f11875317f8a7d791db94cdd82bd02bd1': {
-    name: 'AirSwap Token',
-    address: '0x1ac0d76f11875317f8a7d791db94cdd82bd02bd1',
-    decimals: 4,
-    symbol: 'AST',
-    kind: tokenKinds.ERC20,
-  },
-}
--- tools/metadata/test/metadata.ts
+++ tools/metadata/test/metadata.ts
@@ -1,7 +1,6 @@
 import { expect } from 'chai'
-import * as ethers from 'ethers'
 
-import TokenMetadata from '..'
+import { fetchTokens, findTokenByAddress, findTokensBySymbol } from '../index'
 import {
   chainIds,
   wethAddresses,
@@ -10,115 +9,43 @@ import {
 } from '@airswap/constants'
 
 describe('Metadata: Mainnet', async () => {
-  const provider = ethers.getDefaultProvider('mainnet')
-  const metadata = new TokenMetadata(provider)
+  let tokens
 
   it('fetches all known tokens', async () => {
-    const tokens = await metadata.fetchKnownTokens()
+    tokens = await fetchTokens(chainIds.MAINNET)
     expect(tokens.length).to.not.equal(0)
   })
   it('checks that ETH does not exist', async () => {
-    const byAddress = metadata.getTokensByAddress()
-    expect(byAddress[ADDRESS_ZERO]).to.be.undefined
+    expect(findTokenByAddress(ADDRESS_ZERO, tokens)).to.be.undefined
   })
   it('checks that WETH exists', async () => {
-    const byAddress = metadata.getTokensByAddress()
-    expect(byAddress[wethAddresses[chainIds.MAINNET]]).to.not.be.undefined
-  })
-  it('fetches the airswap token', async () => {
-    metadata.fetchToken(stakingTokenAddresses[chainIds.MAINNET]).then(token => {
-      expect(token.symbol).to.equal('AST')
-    })
+    expect(findTokenByAddress(wethAddresses[chainIds.MAINNET], tokens)).to.not
+      .be.undefined
   })
   it('finds the airswap token', async () => {
-    const tokens = metadata.findTokensBySymbol('AST')
-    expect(tokens[0].address).to.equal(stakingTokenAddresses[chainIds.MAINNET])
+    expect(findTokensBySymbol('AST', tokens)[0].address).to.equal(
+      stakingTokenAddresses[chainIds.MAINNET]
+    )
   })
 })
 
 describe('Metadata: Rinkeby', async () => {
-  const provider = ethers.getDefaultProvider('rinkeby')
-  const metadata = new TokenMetadata(provider)
+  let tokens
 
   it('fetches all known tokens', async () => {
-    metadata.fetchKnownTokens().then(tokens => {
-      expect(tokens.length).to.not.equal(0)
-    })
+    tokens = await fetchTokens(chainIds.RINKEBY)
+    expect(tokens.length).to.not.equal(0)
   })
   it('checks that ETH does not exist', async () => {
-    const byAddress = metadata.getTokensByAddress()
-    expect(byAddress[ADDRESS_ZERO]).to.be.undefined
+    expect(findTokenByAddress(ADDRESS_ZERO, tokens)).to.be.undefined
   })
   it('checks that WETH exists', async () => {
-    const byAddress = metadata.getTokensByAddress()
-    expect(byAddress[wethAddresses[chainIds.RINKEBY]]).to.not.be.undefined
+    expect(findTokenByAddress(wethAddresses[chainIds.RINKEBY], tokens)).to.not
+      .be.undefined
   })
-  it('fetches the airswap token', async () => {
-    const token = await metadata.fetchToken(
+  it('finds the airswap token', async () => {
+    expect(findTokensBySymbol('AST', tokens)[0].address).to.equal(
       stakingTokenAddresses[chainIds.RINKEBY]
     )
-    expect(token.symbol).to.equal('AST')
-  })
-  it('finds the airswap token', async () => {
-    const tokens = metadata.findTokensBySymbol('AST')
-    expect(tokens[0].address).to.equal(stakingTokenAddresses[chainIds.RINKEBY])
-  })
-})
-
-describe('Metadata: Goerli', async () => {
-  const provider = ethers.getDefaultProvider('goerli')
-  const metadata = new TokenMetadata(provider)
-
-  it('fetches all known tokens', async () => {
-    metadata.fetchKnownTokens().then(tokens => {
-      expect(tokens.length).to.not.equal(0)
-    })
-  })
-  it('checks that ETH does not exist', async () => {
-    const byAddress = metadata.getTokensByAddress()
-    expect(byAddress[ADDRESS_ZERO]).to.be.undefined
-  })
-  it('checks that WETH exists', async () => {
-    const byAddress = metadata.getTokensByAddress()
-    expect(byAddress[wethAddresses[chainIds.GOERLI]]).to.not.be.undefined
-  })
-  it('fetches the airswap token', async () => {
-    const token = await metadata.fetchToken(
-      stakingTokenAddresses[chainIds.GOERLI]
-    )
-    expect(token.symbol).to.equal('AST')
-  })
-  it('finds the airswap token', async () => {
-    const tokens = metadata.findTokensBySymbol('AST')
-    expect(tokens[0].address).to.equal(stakingTokenAddresses[chainIds.GOERLI])
-  })
-})
-
-describe('Metadata: Kovan', async () => {
-  const provider = ethers.getDefaultProvider('kovan')
-  const metadata = new TokenMetadata(provider)
-
-  it('fetches all known tokens', async () => {
-    metadata.fetchKnownTokens().then(tokens => {
-      expect(tokens.length).to.not.equal(0)
-    })
-  })
-  it('checks that ETH does not exist', async () => {
-    const byAddress = metadata.getTokensByAddress()
-    expect(byAddress[ADDRESS_ZERO]).to.be.undefined
-  })
-  it('checks that WETH exists', async () => {
-    const byAddress = metadata.getTokensByAddress()
-    expect(byAddress[wethAddresses[chainIds.KOVAN]]).to.not.be.undefined
-  })
-  it('fetches the airswap token', async () => {
-    const token = await metadata.fetchToken(
-      stakingTokenAddresses[chainIds.KOVAN]
-    )
-    expect(token.symbol).to.equal('AST')
-  })
-  it('finds the airswap token', async () => {
-    const tokens = metadata.findTokensBySymbol('AST')
-    expect(tokens[0].address).to.equal(stakingTokenAddresses[chainIds.KOVAN])
   })
 })
