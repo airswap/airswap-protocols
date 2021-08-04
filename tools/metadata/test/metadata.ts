1c8cffffa13e4c705065c82b0d757fdbfdbb5577
hmalik88 <hbmalik88@gmail.com>
2021-08-04 06:00:05 -0400

    updated tests to accomodate for fetchTokens' new return value

--- tools/metadata/test/metadata.ts
+++ tools/metadata/test/metadata.ts
@@ -18,20 +18,20 @@ describe('Metadata: Mainnet', async () => {
 
   it('fetches all known tokens', async () => {
     tokens = await fetchTokens(chainIds.MAINNET)
-    expect(tokens.length).to.not.equal(0)
+    expect(tokens.tokens.length).to.not.equal(0)
   })
   it('checks that ETH does not exist', async () => {
-    expect(findTokenByAddress(ADDRESS_ZERO, tokens)).to.be.undefined
+    expect(findTokenByAddress(ADDRESS_ZERO, tokens.tokens)).to.be.undefined
   })
   it('checks that WETH exists', async () => {
-    expect(findTokenByAddress(wethAddresses[chainIds.MAINNET], tokens)).to.not
-      .be.undefined
+    expect(findTokenByAddress(wethAddresses[chainIds.MAINNET], tokens.tokens))
+      .to.not.be.undefined
   })
   it('finds the AirSwap token', async () => {
-    expect(findTokensBySymbol('AST', tokens)[0].address).to.equal(
+    expect(findTokensBySymbol('AST', tokens.tokens)[0].address).to.equal(
       stakingTokenAddresses[chainIds.MAINNET]
     )
-    expect(firstTokenBySymbol('AST', tokens).address).to.equal(
+    expect(firstTokenBySymbol('AST', tokens.tokens).address).to.equal(
       stakingTokenAddresses[chainIds.MAINNET]
     )
   })
@@ -42,20 +42,20 @@ describe('Metadata: Rinkeby', async () => {
 
   it('fetches all known tokens', async () => {
     tokens = await fetchTokens(chainIds.RINKEBY)
-    expect(tokens.length).to.not.equal(0)
+    expect(tokens.tokens.length).to.not.equal(0)
   })
   it('checks that ETH does not exist', async () => {
-    expect(findTokenByAddress(ADDRESS_ZERO, tokens)).to.be.undefined
+    expect(findTokenByAddress(ADDRESS_ZERO, tokens.tokens)).to.be.undefined
   })
   it('checks that WETH exists', async () => {
-    expect(findTokenByAddress(wethAddresses[chainIds.RINKEBY], tokens)).to.not
-      .be.undefined
+    expect(findTokenByAddress(wethAddresses[chainIds.RINKEBY], tokens.tokens))
+      .to.not.be.undefined
   })
   it('finds the AirSwap token', async () => {
-    expect(findTokensBySymbol('AST', tokens)[0].address).to.equal(
+    expect(findTokensBySymbol('AST', tokens.tokens)[0].address).to.equal(
       stakingTokenAddresses[chainIds.RINKEBY]
     )
-    expect(firstTokenBySymbol('AST', tokens).address).to.equal(
+    expect(firstTokenBySymbol('AST', tokens.tokens).address).to.equal(
       stakingTokenAddresses[chainIds.RINKEBY]
     )
   })
