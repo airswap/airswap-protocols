f8d2d146e8bcd1787c2fb0cd0c395266b73c1277
bold <2985057+AboldUSER@users.noreply.github.com>
2021-09-04 01:21:49 -0700

    Update deploy script to include WETH address, update name of converter test files

--- source/converter/scripts/converter-script.js
+++ source/converter/scripts/converter-script.js
@@ -4,7 +4,7 @@ const hre = require('hardhat')
 async function main() {
   await hre.run('compile')
 
-  const swapToToken = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48' // placeholder token
+  const swapToToken = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2' // placeholder token
   const uniRouter = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D' // placeholder address
   const triggerFee = 1
   const payees = ['0x7296333e1615721f4Bd9Df1a3070537484A50CF8'] // placeholder address
