8a714584b4c52b58c7d3483b3b831a626152f5b4
Greypixel <greypixel@gpxl.tech>
2021-12-14 00:09:26 +0000

    Use ChainLink registry

--- tools/subgraph/src/PricingHelper.ts
+++ tools/subgraph/src/PricingHelper.ts
@@ -4,21 +4,18 @@ import { ERC20 } from '../generated/SwapLightContract/ERC20'
 import { getCollectedFeesDay } from "./EntityHelper"
 import {
   Swap as SwapEvent,
-  SwapLightContract as SwapContract
 } from "../generated/SwapLightContract/SwapLightContract"
 
-let supportedOracles: TypedMap<string, string> = new TypedMap<string, string>()
-supportedOracles.set('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419') // WETH
-supportedOracles.set('0x6b175474e89094c44da98b954eedeac495271d0f', '0x777A68032a88E5A84678A77Af2CD65A7b3c0775a') // DAI
-supportedOracles.set('0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', '0x9211c6b3BF41A10F78539810Cf5c64e1BB78Ec60') // USDC
-supportedOracles.set('0xdac17f958d2ee523a2206206994597c13d831ec7', '0x2ca5A90D34cA333661083F89D831f757A9A50148') // USDT
+const CHAINLINK_FEED_REGISTRY_ADDRESS = "0x47Fb2585D2C56Fe188D0E6ec628a38b74fCeeeDf";
+const USD_DENOMINATION = "0x0000000000000000000000000000000000000348"
 
 function getPrice(tokenAddress: string): BigInt {
-  if (!supportedOracles.isSet(tokenAddress)) {
-    return BigInt.fromI32(0)
-  }
-  let oracleContract = OracleContract.bind(Address.fromString(supportedOracles.get(tokenAddress)!))
-  let roundData = oracleContract.try_latestRoundData()
+  let oracleContract = OracleContract.bind(Address.fromString(CHAINLINK_FEED_REGISTRY_ADDRESS))
+  let roundData = oracleContract.try_latestRoundData(
+    Address.fromString(tokenAddress),
+    Address.fromString(USD_DENOMINATION)
+  )
+
   if (roundData.reverted) {
     return BigInt.fromI32(0)
   }
