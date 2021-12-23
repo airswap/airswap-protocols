9af202181d4b072b0eb7648dbde426c77e5b6c73
francisldn <fnhh04@gmail.com>
2021-12-23 06:39:05 +0000

    updated import statement

--- source/pool/contracts/Pool.sol
+++ source/pool/contracts/Pool.sol
@@ -9,7 +9,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-import ".@airswap/staking/contracts/interfaces/IStaking.sol";
+import "@airswap/staking/contracts/interfaces/IStaking.sol";
 import "./interfaces/IPool.sol";
 
 /**
