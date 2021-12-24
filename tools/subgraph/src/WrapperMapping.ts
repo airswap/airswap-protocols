2f5892a50831e01931f37cd3c558db4805c23d25
Geewynn <ekainugodwin@gmail.com>
2021-12-24 08:14:27 +0100

    removed transfer ownership from wrapper mapping

--- tools/subgraph/src/WrapperMapping.ts
+++ tools/subgraph/src/WrapperMapping.ts
@@ -1,19 +1,8 @@
 import {
-  OwnershipTransferred as OwnershipTransferredEvent,
   WrappedSwapFor as WrappedSwapForEvent
 } from "../generated/Wrapper/Wrapper"
-import { OwnershipTransferred, WrappedSwapFor } from "../generated/schema"
+import { WrappedSwapFor } from "../generated/schema"
 
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-  entity.save()
-}
 
 export function handleWrappedSwapFor(event: WrappedSwapForEvent): void {
   let completedSwapFor = new WrappedSwapFor(
