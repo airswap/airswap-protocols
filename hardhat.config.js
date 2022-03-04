0728c12cf61f7c8f8766fab18c9d36017e4d5b52
Don Mosites <mosites@gmail.com>
2022-03-04 13:21:36 +0100

    Consolidate .env; bump libraries with 3.0.0 beta

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config({ path: '../../.env' })
 require('@nomiclabs/hardhat-ethers')
 require('@nomiclabs/hardhat-waffle')
 require('@nomiclabs/hardhat-etherscan')
