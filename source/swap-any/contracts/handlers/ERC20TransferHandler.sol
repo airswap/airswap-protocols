b60537cc699409fbc5b387542cd4d1ad5545e721
Don Mosites <mosites@gmail.com>
2022-11-28 14:37:50 +0100

    SwapAny: complete configs and missing files; now compiles

--- source/swap-any/contracts/handlers/ERC20TransferHandler.sol
+++ source/swap-any/contracts/handlers/ERC20TransferHandler.sol
@@ -1,28 +1,12 @@
-/*
-  Copyright 2020 Swap Holdings Ltd.
+// SPDX-License-Identifier: MIT
 
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
-
-pragma solidity 0.5.16;
+pragma solidity 0.8.17;
 
 import "../interfaces/ITransferHandler.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20TransferHandler is ITransferHandler {
-  using SafeERC20 for IERC20;
-
   /**
    * @notice Function to wrap safeTransferFrom for ERC20
    * @param from address Wallet address to transfer from
@@ -40,7 +24,7 @@ contract ERC20TransferHandler is ITransferHandler {
     address token
   ) external returns (bool) {
     require(id == 0, "ID_INVALID");
-    IERC20(token).safeTransferFrom(from, to, amount);
+    IERC20(token).transferFrom(from, to, amount);
     return true;
   }
 }
--- source/swap-any/contracts/interfaces/ISwapAny.sol
+++ source/swap-any/contracts/interfaces/ISwapAny.sol
@@ -1,15 +1,4 @@
-/*
-  Copyright 2020 Swap Holdings Ltd.
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
+// SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.17;
 
@@ -140,15 +129,4 @@ interface ISwapAny {
   function signerMinimumNonce(address) external view returns (uint256);
 
   function registry() external view returns (TransferHandlerRegistry);
-
-  /**
-   * @notice Provides hash for a given order
-   * @param order OrderAny
-   * @param domainSeparator bytes32
-   *  */
-
-  function hashOrder(OrderAny calldata order, bytes32 domainSeparator)
-    external
-    view
-    returns (bytes32);
 }
