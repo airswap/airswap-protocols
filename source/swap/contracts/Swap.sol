5beb10239df4fc3374fa394d11ef7485b1fe6e0c
don mosites <mosites@gmail.com>
2022-12-14 22:21:49 +0100

    Simplify Swap.check and update unit tests (#956)

--- source/swap/contracts/Swap.sol
+++ source/swap/contracts/Swap.sol
@@ -322,9 +322,6 @@ contract Swap is ISwap {
       }
     }
 
-    bool success;
-    bytes memory data;
-
     ITransferHandler signerTransferHandler = registry.transferHandlers(
       order.signer.kind
     );
@@ -333,23 +330,11 @@ contract Swap is ISwap {
       errors[errCount] = "SIGNER_TOKEN_KIND_UNKNOWN";
       errCount++;
     } else {
-      (success, data) = address(signerTransferHandler).staticcall(
-        abi.encodeWithSelector(
-          signerTransferHandler.hasAllowance.selector,
-          order.signer
-        )
-      );
-      if (!success || !abi.decode(data, (bool))) {
+      if (!signerTransferHandler.hasAllowance(order.signer)) {
         errors[errCount] = "SIGNER_ALLOWANCE_LOW";
         errCount++;
       }
-      (success, data) = address(signerTransferHandler).staticcall(
-        abi.encodeWithSelector(
-          signerTransferHandler.hasBalance.selector,
-          order.signer
-        )
-      );
-      if (!success || !abi.decode(data, (bool))) {
+      if (!signerTransferHandler.hasBalance(order.signer)) {
         errors[errCount] = "SIGNER_BALANCE_LOW";
         errCount++;
       }
@@ -363,23 +348,11 @@ contract Swap is ISwap {
       errors[errCount] = "SENDER_TOKEN_KIND_UNKNOWN";
       errCount++;
     } else {
-      (success, data) = address(senderTransferHandler).staticcall(
-        abi.encodeWithSelector(
-          senderTransferHandler.hasAllowance.selector,
-          order.sender
-        )
-      );
-      if (!success || !abi.decode(data, (bool))) {
+      if (!senderTransferHandler.hasAllowance(order.sender)) {
         errors[errCount] = "SENDER_ALLOWANCE_LOW";
         errCount++;
       }
-      (success, data) = address(senderTransferHandler).staticcall(
-        abi.encodeWithSelector(
-          senderTransferHandler.hasBalance.selector,
-          order.sender
-        )
-      );
-      if (!success || !abi.decode(data, (bool))) {
+      if (!senderTransferHandler.hasBalance(order.sender)) {
         errors[errCount] = "SENDER_BALANCE_LOW";
         errCount++;
       }
