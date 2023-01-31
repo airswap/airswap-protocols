73a21552a503bc1ff8469825cc73ffe6f6904939
don mosites <mosites@gmail.com>
2023-01-31 10:15:03 +0100

    Reconcile swap and swap-erc20 (#1006)
    * Reconcile differences; swap and swap-erc20
    
    * Remove variable; add comment for hash

--- source/swap/contracts/Swap.sol
+++ source/swap/contracts/Swap.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -8,7 +7,8 @@ import "./interfaces/IAdapter.sol";
 import "./interfaces/ISwap.sol";
 
 /**
- * @title Swap: The Atomic Swap used on the AirSwap Network
+ * @title AirSwap: Atomic Token Swap
+ * @notice https://www.airswap.io/
  */
 contract Swap is ISwap, Ownable, EIP712 {
   bytes32 public constant DOMAIN_TYPEHASH =
@@ -50,17 +50,15 @@ contract Swap is ISwap, Ownable, EIP712 {
       )
     );
 
-  // Data type used for hashing: Structured data (EIP-191)
+  // EIP191 header for use in EIP712 signatures
   bytes internal constant EIP191_HEADER = "\x19\x01";
 
-  // Domain name and version for use in signatures (EIP-712)
+  // Domain name and version for use in EIP712 signatures
   string public constant DOMAIN_NAME = "SWAP";
   string public constant DOMAIN_VERSION = "3";
+
   uint256 public constant FEE_DIVISOR = 10000;
   uint256 internal constant MAX_ERROR_COUNT = 10;
-
-  uint256 public protocolFee;
-  address public protocolFeeWallet;
 
   /**
    * @notice Double mapping of signers to nonce groups to nonce states
@@ -71,6 +69,9 @@ contract Swap is ISwap, Ownable, EIP712 {
 
   mapping(address => address) public override authorized;
 
+  uint256 public protocolFee;
+  address public protocolFeeWallet;
+
   // Mapping of signer addresses to an optionally set minimum valid nonce
   mapping(address => uint256) public _signatoryMinimumNonce;
 
@@ -78,8 +79,8 @@ contract Swap is ISwap, Ownable, EIP712 {
   mapping(bytes4 => IAdapter) public adapters;
 
   /**
-   * @notice Contract Constructor
-   * @dev Sets domain for signature validation (EIP-712)
+   * @notice Constructor
+   * @dev Sets domain and version for EIP712 signatures
    * @param _adapters IAdapter[] array of token adapters
    * @param _protocolFee uin256 fee to be assessed on swaps
    * @param _protocolFeeWallet address destination for fees
@@ -91,8 +92,10 @@ contract Swap is ISwap, Ownable, EIP712 {
   ) EIP712(DOMAIN_NAME, DOMAIN_VERSION) {
     if (_protocolFee >= FEE_DIVISOR) revert InvalidFee();
     if (_protocolFeeWallet == address(0)) revert InvalidFeeWallet();
+
     protocolFee = _protocolFee;
     protocolFeeWallet = _protocolFeeWallet;
+
     if (_adapters.length == 0) revert InvalidAdapters();
     for (uint256 i = 0; i < _adapters.length; i++) {
       adapters[_adapters[i].interfaceID()] = _adapters[i];
@@ -104,25 +107,25 @@ contract Swap is ISwap, Ownable, EIP712 {
    * @param order Order to settle
    */
   function swap(address recipient, Order calldata order) external {
-    // Validate the sender side of the trade.
+    // Validate the signer side of the swap
+    _checkValidOrder(order, _domainSeparatorV4());
+
+    // Validate the sender side of the swap
     address finalSenderWallet;
 
     if (order.sender.wallet == address(0)) {
       /**
        * Sender is not specified. The msg.sender of the transaction becomes
-       * the sender of the order.
+       * the sender of the order
        */
       finalSenderWallet = msg.sender;
     } else {
-      // The msg.sender is authorized.
+      // Ensure that msg.sender is the sender specified on the order
       finalSenderWallet = order.sender.wallet;
       if (order.sender.wallet != msg.sender) revert SenderInvalid();
     }
 
-    // Validate the signer side of the trade.
-    _checkValidOrder(order, _domainSeparatorV4());
-
-    // Transfer token from sender to signer.
+    // Transfer token from sender to signer
     _transferToken(
       finalSenderWallet,
       order.signer.wallet,
@@ -132,7 +135,7 @@ contract Swap is ISwap, Ownable, EIP712 {
       order.sender.kind
     );
 
-    // Transfer token from signer to sender.
+    // Transfer token from signer to recipient
     _transferToken(
       order.signer.wallet,
       recipient,
@@ -142,7 +145,7 @@ contract Swap is ISwap, Ownable, EIP712 {
       order.signer.kind
     );
 
-    // Transfer token from signer to affiliate if specified.
+    // Transfer token from signer to affiliate if specified
     if (order.affiliate.token != address(0)) {
       _transferToken(
         order.signer.wallet,
@@ -154,7 +157,7 @@ contract Swap is ISwap, Ownable, EIP712 {
       );
     }
 
-    // Check if protocol fee is applicable and transfer it accordingly
+    // Check if protocol fee is to be transferred
     if (adapters[order.signer.kind].attemptFeeTransfer()) {
       _transferProtocolFee(order);
     }
@@ -221,38 +224,6 @@ contract Swap is ISwap, Ownable, EIP712 {
   }
 
   /**
-   * @notice Hash an order into bytes32
-   * @dev EIP-191 header and domain separator included
-   * @param order Order The order to be hashed
-   * @param domainSeparator bytes32
-   * @return bytes32 A keccak256 abi.encodePacked value
-   */
-  function _hashOrder(Order calldata order, bytes32 domainSeparator)
-    internal
-    view
-    returns (bytes32)
-  {
-    return
-      keccak256(
-        abi.encodePacked(
-          EIP191_HEADER,
-          domainSeparator,
-          keccak256(
-            abi.encode(
-              ORDER_TYPEHASH,
-              order.nonce,
-              order.expiry,
-              protocolFee,
-              keccak256(abi.encode(PARTY_TYPEHASH, order.signer)),
-              keccak256(abi.encode(PARTY_TYPEHASH, order.sender)),
-              keccak256(abi.encode(PARTY_TYPEHASH, order.affiliate))
-            )
-          )
-        )
-      );
-  }
-
-  /**
    * @notice Cancel one or more nonces
    * @dev Cancelled nonces are marked as used
    * @dev Emits a Cancel event
@@ -289,7 +260,7 @@ contract Swap is ISwap, Ownable, EIP712 {
     uint256 errCount;
     bytes32[] memory errors = new bytes32[](MAX_ERROR_COUNT);
     address signatory = ecrecover(
-      _hashOrder(order, _domainSeparatorV4()),
+      _getOrderHash(order, _domainSeparatorV4()),
       order.v,
       order.r,
       order.s
@@ -377,76 +348,6 @@ contract Swap is ISwap, Ownable, EIP712 {
   }
 
   /**
-   * @notice Tests whether signature and signer are valid
-   * @param order Order to validate
-   * @param domainSeparator bytes32
-   */
-
-  function _checkValidOrder(Order calldata order, bytes32 domainSeparator)
-    internal
-  {
-    // Ensure the order is not expired.
-    if (order.expiry <= block.timestamp) revert OrderExpired();
-
-    bytes32 hashed = _hashOrder(order, domainSeparator);
-
-    // Recover the signatory from the hash and signature
-    address signatory = _getSignatory(order, hashed);
-
-    // Ensure the signatory is not null
-    if (signatory == address(0)) revert SignatureInvalid();
-
-    // Ensure the nonce is not yet used and if not mark it used
-    _markNonceAsUsed(signatory, order.nonce);
-
-    // Ensure the nonce is not yet used and if not mark it used
-    if (order.nonce < _signatoryMinimumNonce[signatory]) revert NonceTooLow();
-
-    // Ensure the signatory is authorized by the signer wallet
-    if (order.signer.wallet != signatory) {
-      if (authorized[order.signer.wallet] != signatory) revert Unauthorized();
-    }
-  }
-
-  /**
-   * @notice Recover the signatory from a signature
-   * @param order Order signeds
-   * @param orderHash hash of the Order signed
-   */
-  function _getSignatory(Order calldata order, bytes32 orderHash)
-    internal
-    pure
-    returns (address)
-  {
-    return ecrecover(orderHash, order.v, order.r, order.s);
-  }
-
-  /**
-   * @notice Perform token transfer for tokens in registry
-   * @dev Transfer type specified by the bytes4 kind param
-   * @dev ERC721: uses transferFrom for transfer
-   * @dev ERC20: Takes into account non-standard ERC-20 tokens.
-   * @param from address Wallet address to transfer from
-   * @param to address Wallet address to transfer to
-   * @param amount uint256 Amount for ERC-20
-   * @param id token ID for ERC-721
-   * @param token address Contract address of token
-   * @param kind bytes4 EIP-165 interface ID of the token
-   */
-  function _transferToken(
-    address from,
-    address to,
-    uint256 amount,
-    uint256 id,
-    address token,
-    bytes4 kind
-  ) internal {
-    IAdapter adapter = adapters[kind];
-    if (address(adapter) == address(0)) revert TokenKindUnknown();
-    adapter.transferTokens(from, to, amount, id, token);
-  }
-
-  /**
    * @notice Returns true if the nonce has been used
    * @param signer address Address of the signer
    * @param nonce uint256 Nonce being checked
@@ -478,6 +379,108 @@ contract Swap is ISwap, Ownable, EIP712 {
     }
 
     _nonceGroups[signatory][groupKey] = group | (uint256(1) << indexInGroup);
+  }
+
+  /**
+   * @notice Tests whether signature and signer are valid
+   * @param order Order to validate
+   * @param domainSeparator bytes32
+   */
+
+  function _checkValidOrder(Order calldata order, bytes32 domainSeparator)
+    internal
+  {
+    // Ensure the order is not expired.
+    if (order.expiry <= block.timestamp) revert OrderExpired();
+
+    bytes32 hashed = _getOrderHash(order, domainSeparator);
+
+    // Recover the signatory from the hash and signature
+    address signatory = _getSignatory(order, hashed);
+
+    // Ensure the signatory is not null
+    if (signatory == address(0)) revert SignatureInvalid();
+
+    // Ensure the nonce is not yet used and if not mark it used
+    _markNonceAsUsed(signatory, order.nonce);
+
+    // Ensure the nonce is not yet used and if not mark it used
+    if (order.nonce < _signatoryMinimumNonce[signatory]) revert NonceTooLow();
+
+    // Ensure the signatory is authorized by the signer wallet
+    if (order.signer.wallet != signatory) {
+      if (authorized[order.signer.wallet] != signatory) revert Unauthorized();
+    }
+  }
+
+  /**
+   * @notice Hash an order into bytes32
+   * @dev EIP-191 header and domain separator included
+   * @param order Order The order to be hashed
+   * @param domainSeparator bytes32
+   * @return bytes32 A keccak256 abi.encodePacked value
+   */
+  function _getOrderHash(Order calldata order, bytes32 domainSeparator)
+    internal
+    view
+    returns (bytes32)
+  {
+    return
+      keccak256(
+        abi.encodePacked(
+          EIP191_HEADER,
+          domainSeparator,
+          keccak256(
+            abi.encode(
+              ORDER_TYPEHASH,
+              order.nonce,
+              order.expiry,
+              protocolFee,
+              keccak256(abi.encode(PARTY_TYPEHASH, order.signer)),
+              keccak256(abi.encode(PARTY_TYPEHASH, order.sender)),
+              keccak256(abi.encode(PARTY_TYPEHASH, order.affiliate))
+            )
+          )
+        )
+      );
+  }
+
+  /**
+   * @notice Recover the signatory from a signature
+   * @param order Order signeds
+   * @param hash hash of the Order signed
+   */
+  function _getSignatory(Order calldata order, bytes32 hash)
+    internal
+    pure
+    returns (address)
+  {
+    return ecrecover(hash, order.v, order.r, order.s);
+  }
+
+  /**
+   * @notice Perform token transfer for tokens in registry
+   * @dev Transfer type specified by the bytes4 kind param
+   * @dev ERC721: uses transferFrom for transfer
+   * @dev ERC20: Takes into account non-standard ERC-20 tokens.
+   * @param from address Wallet address to transfer from
+   * @param to address Wallet address to transfer to
+   * @param amount uint256 Amount for ERC-20
+   * @param id token ID for ERC-721
+   * @param token address Contract address of token
+   * @param kind bytes4 EIP-165 interface ID of the token
+   */
+  function _transferToken(
+    address from,
+    address to,
+    uint256 amount,
+    uint256 id,
+    address token,
+    bytes4 kind
+  ) internal {
+    IAdapter adapter = adapters[kind];
+    if (address(adapter) == address(0)) revert TokenKindUnknown();
+    adapter.transferTokens(from, to, amount, id, token);
   }
 
   /**
