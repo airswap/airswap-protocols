d799924c1e50dd8705d789ae3a85ff26e35ce929
Smart Contrart <smartcontrart@protonmail.com>
2023-02-02 13:04:07 -0500

    Removed unused variables

--- source/swap/contracts/Swap.sol
+++ source/swap/contracts/Swap.sol
@@ -259,7 +259,7 @@ contract Swap is ISwap, Ownable, EIP712 {
   {
     uint256 errCount;
     bytes32[] memory errors = new bytes32[](MAX_ERROR_COUNT);
-    (address signatory, ECDSA.RecoverError err) = ECDSA.tryRecover(
+  (address signatory,) = ECDSA.tryRecover(
       _getOrderHash(order, _domainSeparatorV4()),
       order.v,
       order.r,
@@ -457,12 +457,7 @@ contract Swap is ISwap, Ownable, EIP712 {
     bytes32 r,
     bytes32 s
   ) internal pure returns (address) {
-    (address signatory, ECDSA.RecoverError err) = ECDSA.tryRecover(
-      hash,
-      v,
-      r,
-      s
-    );
+    (address signatory,) = ECDSA.tryRecover(hash, v, r, s);
     return signatory;
   }
 
