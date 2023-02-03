a8afa3c28fba3b46402db0ad6133cd4dba3e6136
Don Mosites <mosites@gmail.com>
2023-02-03 16:06:36 +0100

    Use Ownable2Step for Swap, SwapERC20, Wrapper

--- packages/swap-erc20/contracts/SwapERC20.sol
+++ packages/swap-erc20/contracts/SwapERC20.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable2Step.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./interfaces/ISwapERC20.sol";
 
@@ -9,7 +9,7 @@ import "./interfaces/ISwapERC20.sol";
  * @title AirSwap: Atomic ERC20 Token Swap
  * @notice https://www.airswap.io/
  */
-contract SwapERC20 is ISwapERC20, Ownable {
+contract SwapERC20 is ISwapERC20, Ownable2Step {
   using SafeERC20 for IERC20;
 
   bytes32 public constant DOMAIN_TYPEHASH =
