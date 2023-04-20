d65eefe8e0aa578ab06af01a9c7421c681938ede
Smart Contrart <smartcontrart@protonmail.com>
2023-04-20 10:26:20 -0400

    Removed unused import

--- source/staking/contracts/Staking.sol
+++ source/staking/contracts/Staking.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/math/Math.sol";
 import "./interfaces/IStaking.sol";
 
 /**
