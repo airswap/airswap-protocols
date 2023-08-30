5e97a59edd5c8894b69fab960b823bf5b69599e2
Don Mosites <mosites@gmail.com>
2023-08-30 15:36:57 +0200

    update protocol interfaces and ids

---
+++ tools/constants/test/utils.ts
@@ -0,0 +1,15 @@
+import { expect } from 'chai'
+import { protocolInterfaces, protocolNames } from '../index'
+
+import { getInterfaceId } from '@airswap/utils'
+
+describe('Constants', async () => {
+  it('InterfaceIds are correct', async () => {
+    for (const interfaceId in protocolInterfaces) {
+      expect(getInterfaceId(protocolInterfaces[interfaceId])).to.be.equal(
+        interfaceId
+      )
+      console.log(protocolNames[interfaceId])
+    }
+  })
+})
