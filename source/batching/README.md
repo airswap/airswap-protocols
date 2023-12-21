b498c1bc3e9650941d14bcc9710b88cd6a38b075
Smart Contrart <smartcontrart@protonmail.com>
2023-12-21 06:46:54 -0500

    Imported Order Checking to Balances and renamed contract to Batching

---
+++ source/batching/README.md
@@ -0,0 +1,35 @@
+# BalanceChecker
+
+[AirSwap](https://www.airswap.io/) is a peer-to-peer trading network for Ethereum tokens. This package contains source code and tests for a basic ERC20 balance and allowance aggregator.
+
+[![Discord](https://img.shields.io/discord/590643190281928738.svg)](https://discord.gg/ecQbV7H)
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+![Twitter Follow](https://img.shields.io/twitter/follow/airswap?style=social)
+
+## Resources
+
+- Docs → https://docs.airswap.io/
+- Website → https://www.airswap.io/
+- Blog → https://blog.airswap.io/
+- Support → https://support.airswap.io/
+
+## Usage
+
+:warning: This package is under active development. The [BalanceChecker](./contracts/BalanceChecker.sol) contract is deployed; see [deploys.js](./deploys.js) for latest. For all AirSwap contract deployments see [Deployed Contracts](https://docs.airswap.io/system/contract-deployments).
+
+## Commands
+
+| Command        | Description                             |
+| :------------- | :-------------------------------------- |
+| `yarn`         | Install dependencies                    |
+| `yarn clean`   | Delete the contract `build` folder      |
+| `yarn compile` | Compile all contracts to `build` folder |
+| `yarn test`    | Run all contract tests in `test` folder |
+
+## Running Tests
+
+:bulb: Prior to testing locally, run `yarn compile` in the `airswap-protocols` project root to build required artifacts. Then run an instance of `ganache-cli` before running `yarn test` in another shell from the root repository.
+
+```
+yarn ganache
+```
---
+++ source/batching/scripts/deploy.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-console */
+const fs = require('fs')
+const Confirm = require('prompt-confirm')
+const { ethers, run } = require('hardhat')
+const { ChainIds, chainNames, chainLabels } = require('@airswap/constants')
+const { getReceiptUrl } = require('@airswap/utils')
+const balancesDeploys = require('../deploys.js')
+
+async function main() {
+  await run('compile')
+  const [deployer] = await ethers.getSigners()
+  const gasPrice = await deployer.getGasPrice()
+  const chainId = await deployer.getChainId()
+  if (chainId === ChainIds.HARDHAT) {
+    console.log('Value for --network flag is required')
+    return
+  }
+  console.log(`Deployer: ${deployer.address}`)
+  console.log(`Network: ${chainNames[chainId].toUpperCase()}`)
+  console.log(`Gas price: ${gasPrice / 10 ** 9} gwei\n`)
+
+  const prompt = new Confirm('Proceed to deploy?')
+  if (await prompt.run()) {
+    const balanceCheckerFactory = await ethers.getContractFactory(
+      'BalanceChecker'
+    )
+    const balanceCheckerContract = await balanceCheckerFactory.deploy()
+    console.log(
+      'Deploying...',
+      getReceiptUrl(chainId, balanceCheckerContract.deployTransaction.hash)
+    )
+    await balanceCheckerContract.deployed()
+    console.log(`Deployed: ${balanceCheckerContract.address}`)
+
+    balancesDeploys[chainId] = balanceCheckerContract.address
+    fs.writeFileSync(
+      './deploys.js',
+      `module.exports = ${JSON.stringify(balancesDeploys, null, '\t')}`
+    )
+    console.log('Updated deploys.js')
+
+    console.log(
+      `\nVerify with "yarn verify --network ${chainLabels[
+        chainId
+      ].toLowerCase()}"\n`
+    )
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
