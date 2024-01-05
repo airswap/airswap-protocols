3157a712f3b992cf2a2f77950a9b0a12e0462813
Smart Contrart <smartcontrart@protonmail.com>
2024-01-05 09:38:28 -0500

    Created owner and deploys-blocks scripts

--- source/batch-call/scripts/owner.js
+++ source/batch-call/scripts/owner.js
@@ -1,1 +1,16 @@
-console.log(`\n✘ Balances is not ownable.\n`)
+const { check } = require('../../../scripts/owners-update')
+const {
+  BatchCall__factory,
+} = require('@airswap/batch-call/typechain/factories/contracts')
+const deploys = require('../deploys.js')
+
+async function main() {
+  await check('BatchCall', BatchCall__factory, deploys)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
