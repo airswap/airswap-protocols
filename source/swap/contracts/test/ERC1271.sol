a23a3b914f7be0d9da833372f5813dfdea411e0b
Smart Contrart <smartcontrart@protonmail.com>
2024-01-21 06:58:06 -0500

    Removed debugging code

--- source/swap/contracts/test/ERC1271.sol
+++ source/swap/contracts/test/ERC1271.sol
@@ -6,54 +6,10 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 contract ERC1271 {
   using SafeERC20 for IERC20;
 
-  constructor() payable {}
-
-  function approve(address token, address swap, uint256 amount) public {
-    IERC20(token).approve(swap, amount);
-  }
-
   function isValidSignature(
     bytes32 _hash,
     bytes memory _signature
   ) public view returns (bytes4) {
     return 0x1626ba7e;
   }
-
-  function testSwap(
-    ISwap swapContract,
-    address recipient,
-    uint256 maxRoyalty,
-    ISwap.Order calldata order
-  ) external {
-    swapContract.swap(recipient, maxRoyalty, order);
-  }
-
-  // function testSwapAnySender(
-  //     ISwap swapContract,
-  //     address recipient,
-  //     uint256 nonce,
-  //     uint256 expiry,
-  //     address signerWallet,
-  //     address signerToken,
-  //     uint256 signerAmount,
-  //     address senderToken,
-  //     uint256 senderAmount,
-  //     uint8 v,
-  //     bytes32 r,
-  //     bytes32 s
-  // ) external {
-  //     swapContract.swapAnySender(
-  //         address(this),
-  //         nonce,
-  //         expiry,
-  //         signerWallet,
-  //         signerToken,
-  //         signerAmount,
-  //         senderToken,
-  //         senderAmount,
-  //         v,
-  //         r,
-  //         s
-  //     );
-  // }
 }
